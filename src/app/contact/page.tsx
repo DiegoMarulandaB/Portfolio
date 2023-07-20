062de01c1ed7004c0be6e1980cc33543dd81dcaf
Diego Marulanda B <56690521+DiegoMarulandaB@users.noreply.github.com>
2023-07-20 15:07:18 -0500

    new measures were added between contents in the home_about_projects

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,10 +8,10 @@ const Contact: React.FC = () => {
     <div className="bg-white dark:bg-slate-800 py-8 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
         <div className="flex flex-col text-center items-center justify-center mt-32">
-          <h2 className="text-2xl text-center sm:text-1xl md:text-2xl lg:text-4xl xl:text-8xl font-bold mb-4 mt-2">
+          <h2 className="text-2xl text-center sm:text-1xl md:text-2xl lg:text-4xl xl:text-8xl font-bold mb-4 ">
             <AnimatedText
               text="Contact me"
-              className="text-1xl text-left sm:text-1xl md:text-4xl lg:text-7xl xl:text-8xl mr-2 mb-2 font-bold"
+              className="text-1xl text-left sm:text-1xl md:text-4xl lg:text-7xl xl:text-8xl mr-2 mb-2  mt-2 font-bold"
             />
           </h2>
           <p className="bg-white dark:bg-slate-800 sm:text-1xl md:text-1xl lg:text-1xl xl:text-4xl mb-4 mt-10">
