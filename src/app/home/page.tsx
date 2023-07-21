89fe8985b140a0d3ae61a261bebb180f39b3f241
Diego Marulanda B <56690521+DiegoMarulandaB@users.noreply.github.com>
2023-07-21 14:59:08 -0500

    add centered buttons_new

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -52,9 +52,9 @@ const HomePage = () => {
             hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-sky-300
             //forma boton
             text-center inline-flex items-center 
-            hover:-translate-y-1 transition-transform cursor-pointer text-white bg-gradient-to-r from-sky-500 via-sky-600 to-sky-700 rounded-r-lg px-3 py-3 mb-8 w-40"
+            hover:-translate-y-1 transition-transform cursor-pointer text-white bg-gradient-to-r from-sky-500 via-sky-600 to-sky-700 rounded-r-lg px-3 py-3 mb-8 w-44"
             >
-              <AiOutlineUser className="icon text-white text-center items-center justify-center  mb-1 mr-1" size={30} />
+              <AiOutlineUser className="icon text-white text-center items-center justify-center  mb-1 mr-2" size={30} />
               About me
             </button>
           </Link>
@@ -92,9 +92,9 @@ const HomePage = () => {
             hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-sky-300
             //forma boton
             text-center inline-flex items-center
-            hover:-translate-y-1 transition-transform cursor-pointer text-white bg-gradient-to-r from-sky-500 via-sky-600 to-sky-700 rounded-r-lg px-3 py-3  mb-4 w-40"
+            hover:-translate-y-1 transition-transform cursor-pointer text-white bg-gradient-to-r from-sky-500 via-sky-600 to-sky-700 rounded-r-lg px-3 py-3  mb-4 w-44"
             >
-              <AiOutlineCode className="icon text-white text-center items-center justify-center  mb-1 mr-1" size={30} />
+              <AiOutlineCode className="icon text-white text-center items-center justify-center  mb-1 mr-2" size={30} />
               My portfolio
             </button>
           </Link>
@@ -123,8 +123,8 @@ const HomePage = () => {
         </div>
       </div>
 
-      <div className="flex flex-col text-center items-end justify-center space-x-4 mt-6 mr-44 mx-64">
-        {ButtonContact.map((project, index) => (
+          <div className="flex flex-col text-center items-end justify-center align-bottom space-x-4 mt-6 mr-44 mx-64 ">
+        {ButtonProjects.map((project, index) => (
           <Link key={index} href={project.route}>
             <button
               type="button"
@@ -132,10 +132,10 @@ const HomePage = () => {
             hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-sky-300
             //forma boton
             text-center inline-flex items-center
-            hover:-translate-y-1 transition-transform cursor-pointer text-white bg-gradient-to-r from-sky-500 via-sky-600 to-sky-700 rounded-r-lg px-3 py-3 mb-4 mx-2 mt-2 w-40"
+            hover:-translate-y-1 transition-transform cursor-pointer text-white bg-gradient-to-r from-sky-500 via-sky-600 to-sky-700 rounded-r-lg px-3 py-3  mb-4 w-44"
             >
-              <AiOutlineMail className="icon text-white text-center items-center justify-center  mb-1 mr-1" size={30} />
-              Contact me
+             <AiOutlineMail className="icon text-white text-center items-center justify-center  mb-1 mr-2" size={30} />
+              My portfolio
             </button>
           </Link>
         ))}
