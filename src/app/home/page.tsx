856917827e7de0b78c2517729777e2c2a3fc9979
Diego Marulanda B <56690521+DiegoMarulandaB@users.noreply.github.com>
2023-07-24 10:33:33 -0500

    add improvements to home cards

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -28,15 +28,15 @@ const HomePage = () => {
         <div className="bg-slate-200 dark:bg-slate-600  rounded-md  shadow-md p-5">
           <div className="flex flex-col items-center justify-center">
             <Image src="/diego.jpg" alt="Profile picture" width={128} height={128} className="w-32 h-32 rounded-full mx-auto" />
-            <h2 className="font-semibold text-2xl mt-3">Hello, I'm Diego!</h2>
-            <p className="text-cyan-600">Frontend Developer</p>
+            <h2 className="text-center font-semibold text-2xl mt-4">Hello, I'm Diego!</h2>
+            <p className="text-cyan-600 mt-1">Frontend Developer</p>
             <div className="mt-5">
               <h3 className="font-semibold text-center text-2xl">Bio</h3>
               <p className= 'text-center mt-2'>
                 <strong>Indie frontend developer based in Colombia!</strong> Passionate about technology, geopolitics, general culture, and science.
               </p>
             </div>
-            <div className="mt-6">
+            <div className="mt-8">
               {ButtonAbout.map((button, index) => (
                 <Link key={index} href={button.route}>
                   <button
@@ -56,11 +56,11 @@ const HomePage = () => {
         <div className="bg-slate-200 dark:bg-slate-600 rounded-md  shadow-md p-5">
           <div className="flex flex-col items-center justify-center">
             <Image src="/projects.svg" alt="Projects image" width={128} height={128} className="w-32 h-32 rounded-full mx-auto" />
-            <h2 className="font-semibold text-2xl mt-3">Projects</h2>
+            <h2 className="font-semibold text-2xl mt-4">Projects</h2>
             <div className="mt-5">
               <p className= 'text-center mt-2'>You can find samples of my projects. I hope you enjoy exploring my work as much as I enjoy creating it!</p>
             </div>
-            <div className="mt-6">
+            <div className="mt-8">
               {ButtonProjects.map((button, index) => (
                 <Link key={index} href={button.route}>
                   <button
@@ -80,18 +80,18 @@ const HomePage = () => {
         <div className="bg-slate-200 dark:bg-slate-600 rounded-md  shadow-md p-5">
           <div className="flex flex-col items-center justify-center">
             <Image src="/email.svg" alt="Email image" width={128} height={128} className="w-32 h-32 rounded-full mx-auto" />
-            <h2 className="font-semibold text-2xl mt-3">Contact me</h2>
+            <h2 className="font-semibold text-2xl mt-4">Contact me</h2>
             <div className="mt-5">
               <p className= 'text-center mt-2'>Feel free to contact me to share the details of your project and explore how we can work together to achieve great results!</p>
             </div>
-            <div className="mt-6">
+            <div className="mt-8">
               {ButtonContact.map((button, index) => (
                 <Link key={index} href={button.route}>
                   <button
                     type="button"
                     className="hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-sky-300 text-center inline-flex items-center hover:-translate-y-1 transition-transform cursor-pointer text-white bg-gradient-to-r from-cyan-500 via-cyan-600 to-cyan-700 rounded-md px-3 py-3 mb-4 md:mb-0 w-full md:w-40"
                   >
-                    {index === 0 && <AiOutlineMail className="icon text-white text-center items-center justify-center mb-1 mr-2" size={32} />}
+                    {index === 0 && <AiOutlineMail className="icon text-white text-center items-center justify-center mb-1 mr-2 " size={32} />}
                     {button.label}
                   </button>
                 </Link>
