e81c8827e51399a8c5cf713faced70c049b5fd08
Diego Marulanda B <56690521+DiegoMarulandaB@users.noreply.github.com>
2023-08-01 07:19:19 -0500

    add fixes to_contact_cards

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -38,11 +38,11 @@ const ContactLink: React.FC<ContactLinkProps> = ({ href, text, icon, username })
       href={href}
       target="_blank"
       rel="noopener dns-prefetch"
-      className="flex flex-col items-center text-center px-4 py-6 border  cursor-pointer text-white bg-gradient-to-r from-cyan-500 via-cyan-600 to-cyan-700  rounded-md"
+      className="flex flex-col items-center text-center px-4 py-6 border transform transition-transform cursor-pointer text-white bg-gradient-to-r from-cyan-500 via-cyan-600 to-cyan-700  rounded-md"
     >
       {icon}
       <span className="text-2xl font-medium text-white">{text}</span>
-      {username && <span className="text-3xl font-bold text-white">{username}</span>}
+      {username && <span className="text-2xl font-bold text-white">{username}</span>}
     </a>
   )
 }
