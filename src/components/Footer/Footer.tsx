f027d47865b0260142d71967ca42ad3ea03d2f5f
Diego Marulanda B <56690521+DiegoMarulandaB@users.noreply.github.com>
2023-08-05 13:44:33 -0500

    fix: :fire: add fixes in the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,11 +3,14 @@ import { AiOutlineGithub, AiOutlineTwitter, AiOutlineLinkedin } from 'react-icon
 
 const Footer = () => {
   return (
-    <section id="footer" className=" mx-auto max-w-full md:max-w-full  px-1 sm:px-1  bg-slate-100  dark:bg-slate-700 mt-12">
+    <section
+      id="footer"
+      className=" mx-auto max-w-full md:max-w-full  px-1 sm:px-1  bg-slate-100  dark:bg-slate-700 mt-12"
+    >
       <hr className="border border-black dark:border-white" />
       <div className="mx-auto p-4 flex flex-col items-center justify-center text-center md:flex-row md:justify-evenly mt-1">
-        <div className="flex flex-row items-center justify-between space-x-2 mb-2 mt-1 mx-14 mr-14 text-neutral-800 dark:text-neutral-100 ">
-          &copy; {new Date().getFullYear()} Diego Marulanda.  All Rights Reserved.
+        <div className="flex flex-row items-center justify-between space-x-2 mb-2 mt-1 mx-16 mr-16 text-neutral-800 dark:text-neutral-100 ">
+          &copy; {new Date().getFullYear()} Diego Marulanda. All Rights Reserved.
           <a href="/" className="hover:underline" />
         </div>
         <div className="flex flex-row items-center justify-between space-x-2 mb-2 mt-1 mx-1 mr-1">
