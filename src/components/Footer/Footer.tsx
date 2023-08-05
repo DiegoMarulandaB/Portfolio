79d7365071fecd640426806e525a4199b5f64a13
Diego Marulanda B <56690521+DiegoMarulandaB@users.noreply.github.com>
2023-08-05 16:08:05 -0500

    fix: :fire: add_features_in_the_footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,11 +9,11 @@ const Footer = () => {
     >
       <hr className="border border-black dark:border-white" />
       <div className="mx-auto p-4 flex flex-col items-center justify-center text-center md:flex-row md:justify-evenly">
-        <div className="flex flex-row items-center justify-start space-x-2   mt-2 mx-16 mr-16   text-neutral-800 dark:text-neutral-100">
+        <div className="flex flex-row items-center justify-start space-x-2   mt-2 mx-14 mr-14   text-neutral-800 dark:text-neutral-100">
           &copy; {new Date().getFullYear()} Diego Marulanda. | All Rights Reserved.
           <a href="/" className="hover:underline" />
         </div>
-        <div className="flex flex-row items-center justify-end space-x-2  mt-2  mx-16 mr-16">
+        <div className="flex flex-row items-center justify-end space-x-2  mt-2  mx-18 mr-18">
           <a href="https://twitter.com/DAMB1987" target="_blank" rel="noopener noreferrer">
             <AiOutlineTwitter
               className="icon  text-white bg-gray-700 dark:bg-gray-700 border-2 border-black dark:border-white rounded-md"
