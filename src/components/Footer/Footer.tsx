0e4419cfe7a0c00a376c08f9f93c4f3e979d9816
Diego Marulanda B <56690521+DiegoMarulandaB@users.noreply.github.com>
2023-08-05 16:27:36 -0500

    fix: :fire: add fixes_copyright_footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <hr className="border border-black dark:border-white" />
       <div className="mx-auto p-4 flex flex-col items-center justify-center text-center md:flex-row md:justify-evenly">
         <div className="flex flex-row items-center justify-start space-x-2   mt-2 mx-18 mr-18   text-neutral-800 dark:text-neutral-100">
-          &copy; {new Date().getFullYear()} Diego Marulanda. | All Rights Reserved.
+          &copy; {new Date().getFullYear()} Diego Marulanda. All Rights Reserved.
           <a href="/" className="hover:underline" />
         </div>
         <div className="flex flex-row items-center justify-end space-x-2  mt-2  mx-18 mr-18">
