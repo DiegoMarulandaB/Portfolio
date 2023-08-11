b812938bef85aece93351dfabe4685396d076191
Diego Marulanda B <56690521+DiegoMarulandaB@users.noreply.github.com>
2023-08-11 09:45:40 -0500

    style: :sparkles: add meta in the head for seo improvements in the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,10 @@ import { ThemeProvider } from 'next-themes'
 
 const metadata: Metadata = {
   title: 'Diego Marulanda',
-  description: 'Diego Marulanda Portfolio',
+  description:
+    'Author Diego Marulanda,Portfolio where you will find different projects, whether academic or personal, related to programming.',
   keywords:
-    'Next.js 13,React,TypeScript,Tailwind, Frontend Developer, Html, Css, JavaScript, Git, Github, Linux, Visual Studio Code, Bash',
+    'Next.js 13,React,TypeScript,Tailwind, Frontend Developer, Html, Css, JavaScript, Git, Github, Linux, Neovim,Visual Studio Code, Bash',
 }
 
 export const popps = Poppins({
@@ -26,6 +27,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="icon" href="./favicon.ico" type="./favicon.ico" sizes="32x32" />
         <title>DiegoMarulanda</title>
+        <meta
+          name="description"
+          content="Author Diego Marulanda,Portfolio where you will find different projects, whether academic or personal, related to programming"
+        ></meta>
       </head>
       <body className={`${popps.className} bg-slate-100 dark:bg-slate-700`}>
         <ThemeProvider enableSystem={true} attribute="class">
