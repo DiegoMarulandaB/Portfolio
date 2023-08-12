a270cf9d9db2838145d38d886891983ec160ec0b
Diego Marulanda B <56690521+DiegoMarulandaB@users.noreply.github.com>
2023-08-12 10:50:37 -0500

    style: :sparkles: add features footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,14 +4,13 @@ import { AiOutlineGithub, AiOutlineTwitter, AiOutlineLinkedin } from 'react-icon
 const Footer = () => {
   return (
     <section id="footer">
-      <div className=" mx-auto max-w-full md:max-w-full  px-1 sm:px-1  mt-12">
+      <div className=" mx-auto max-w-full md:max-w-full px-1 sm:px-1  mt-12">
         <hr className="border border-black dark:border-white" />
-        <div className="mx-auto p-4 flex flex-col items-center justify-center text-center md:flex-row md:justify-evenly">
-          <div className="flex flex-row items-center justify-start space-x-2   mt-2 mx-18 mr-18   text-neutral-800 dark:text-neutral-100">
-            &copy; {new Date().getFullYear()} Diego Marulanda. All Rights Reserved.
-            <a href="/" className="hover:underline" />
+        <div className="mx-auto p-2 flex flex-col items-center justify-center text-center md:flex-row md:justify-evenly">
+          <div className="flex flex-row items-center justify-center space-x-2   mt-2   text-neutral-800 dark:text-neutral-100 mx-18 mr-18">
+            &copy; 2023 Diego Marulanda. All Rights Reserved.
           </div>
-          <div className="flex flex-row items-center justify-end space-x-2  mt-2  mx-18 mr-18">
+          <div className="flex flex-row items-center justify- space-x-2 mt-2 mx-18 mr-18">
             <a
               href="https://twitter.com/DAMB1987"
               target="_blank"
