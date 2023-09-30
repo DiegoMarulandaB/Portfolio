d256621f6dfdb776d5df7d83bf1eebb4b2a24e78
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2023-09-30 11:16:24 -0500

    style: :lipstick: add new size of contact cards

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,11 +11,11 @@ const Contact = () => {
           <h1 className="text-center mb-4">
             <AnimatedText text="Contact me" className="mr-2 mb-2  mt-2" />
           </h1>
-          <p className="bg-slate-100 dark:bg-slate-700 mb-4 mt-4 text-center mx-2 mr-2 text-2xl text-pink-600">
+          <p className="bg-slate-100 dark:bg-slate-700 mb-4 mt-6 text-center mx-2 mr-2 text-2xl text-pink-600">
             Get in touch with me through these links!
           </p>
         </div>
-        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4 mt-4 mx-2 mr-2">
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 lg:grid-cols-2 mt-6 mx-2 mr-2">
           {contactLinks.map((link) => (
             <ContactLink key={link.text} {...link} />
           ))}
@@ -36,7 +36,7 @@ const ContactLink: React.FC<ContactLinkProps> = ({ href, text, username }) => (
     href={href}
     target="_blank"
     rel="noopener noreferrer"
-    className="flex flex-col items-center text-center px-4 py-4 border transform transition-transform cursor-pointer text-white bg-gradient-to-r from-cyan-500 via-cyan-600 to-cyan-700  rounded-md"
+    className="flex flex-col items-center text-center px-4 py-4 border transform transition-transform cursor-pointer text-white bg-gradient-to-r from-cyan-500 via-cyan-600 to-cyan-700 rounded-md"
   >
     <span className="text-2xl font-medium text-white">{text}</span>
     {username && <span className="text-1xl font-bold text-white">{username}</span>}
