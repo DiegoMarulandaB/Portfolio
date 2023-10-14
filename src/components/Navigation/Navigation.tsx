ed0a149548f314a9c3b9836d9d8e1fa3904b0a89
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2023-10-14 16:38:02 -0500

    style: Add modifications to navigation and footer borders

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -23,10 +23,10 @@ export default function Navigation() {
 
   
   return (
-    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-slate-100 dark:bg-slate-700 dark:border-b dark:border-slate-700 ">
-      <div className="justify-evenly  md:items-center md:flex  ">
+    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-slate-100 dark:bg-slate-700  dark:border-slate-600 ">
+      <div className="justify-between  md:items-center md:flex  ">
         <div>
-          <div className="flex items-center justify-between py-3 md:py-5 md:block ">
+          <div className="flex items-center justify-evenly py-3 md:py-5 md:block ">
             <Link href="/" rel="noopener noreferrer dns-prefetch">
               <div className="container flex items-center space-x-2">
                 <h2 className="text-1xl font-bold transition-transform cursor-pointer mx-2 mr-2">Diego Marulanda</h2>
