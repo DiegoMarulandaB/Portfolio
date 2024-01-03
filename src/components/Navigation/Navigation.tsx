a61c319a777377bef9333e3d8a8ceececb9c2251
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-03 10:33:44 -0500

    refactor: :lipstick: add bew UI navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -24,10 +24,10 @@ const Navigation = () => {
   const [navigation, setNavigation] = useState(false)
 
   return (
-    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-slate-100 dark:bg-slate-700  dark:border-slate-600 ">
+    <header className="w-full mx-auto  px-6 sm:px-20 opacity-90 fixed top-0 z-50 shadow bg-slate-100 dark:bg-slate-700  dark:border-slate-600 ">
       <div className="justify-evenly  md:items-center md:flex  ">
         <div>
-          <div className="flex items-center justify-between py-3 md:py-5 md:block ">
+          <div className="flex items-center justify-between py-3 md:py-6 md:block ">
             <Link to="home">
               <div className="container flex items-center space-x-2">
                 <h2 className="text-1xl font-bold transition-transform  cursor-pointer mx-2 mr-2">Diego Marulanda</h2>
