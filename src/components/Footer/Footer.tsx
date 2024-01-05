892c142b2332bd5de9dad7a765697328d042d560
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-05 11:08:36 -0500

    refactor: :bug: add fixes footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import { RiTwitterXFill } from 'react-icons/ri'
 const Footer = () => {
   return (
     <section id="footer">
-      <div className=" mx-auto max-w-full md:max-w-full  px-1 sm:px-1  mt-28  bg-slate-100 dark:bg-slate-700 ">
+      <div className=" mx-auto max-w-full md:max-w-full  px-1 sm:px-1  mt-24  bg-slate-100 dark:bg-slate-700 ">
         <hr className=" dark:border-slate-500 " />
         <div className="mx-auto p-2 flex flex-col items-center justify-center text-center md:flex-row md:justify-evenly">
           <div className="flex flex-row items-center justify-evenly space-x-2   mt-2 mx-16 mr-16 ">
