422fed33e16995f50516b0079681e53be7b7ef91
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-05 08:17:43 -0500

    refactor: :bug: add fixes footer_home

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,13 +5,13 @@ import { RiTwitterXFill } from 'react-icons/ri'
 const Footer = () => {
   return (
     <section id="footer">
-      <div className=" mx-auto max-w-full md:max-w-full  px-1 sm:px-1  mt-24  bg-slate-100 dark:bg-slate-700 ">
+      <div className=" mx-auto max-w-full md:max-w-full  px-1 sm:px-1  mt-28  bg-slate-100 dark:bg-slate-700 ">
         <hr className=" dark:border-slate-500 " />
         <div className="mx-auto p-2 flex flex-col items-center justify-center text-center md:flex-row md:justify-evenly">
-          <div className="flex flex-row items-center justify-evenly space-x-2   mt-2 mx-16 mr-16 ">
+          <div className="flex flex-row items-center justify-evenly space-x-2   mt-2 mx-20 mr-16 ">
             &copy; 2023-2024 Diego Marulanda. All Rights Reserved.
           </div>
-          <div className="flex flex-row items-center justify-evenly space-x-2 mt-2 mx-18 mr-18">
+          <div className="flex flex-row items-center justify-evenly space-x-2 mt-2 mx-16 mr-20">
             <a
               href="https://twitter.com/DAMB1987"
               className=" hover:text-purple-950 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125"
