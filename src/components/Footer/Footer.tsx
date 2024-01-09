52614a1b082ccaa0116c5734c7506931ee1974b0
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-09 08:45:11 -0500

    refactor: :fire: add fix_footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,11 +5,11 @@ const Footer = () => {
     <section id="footer">
       <div className=" mx-auto max-w-full md:max-w-full px-1 sm:px-1 mt-24  bg-slate-100 dark:bg-slate-700">
         <hr className=" dark:border-slate-500 " />
-        <div className="mx-auto p-2 flex flex-col items-center justify-center text-center md:flex-row md:justify-between ml-24 mr-16">
-          <div className="flex flex-row space-x-2 mt-2">
+        <div className="mx-auto p-2 flex flex-col items-center justify-center text-center md:flex-row md:justify-between">
+          <div className="flex flex-row space-x-2 mt-2 ml-16 mr-16">
             &copy; 2023-2024 Diego Marulanda. All Rights Reserved.
           </div>
-          <div className="flex flex-row space-x-2 mt-2">
+          <div className="flex flex-row space-x-2 mt-2 ml-16 mr-16">
             <a
               href="mailto:diegomarulanda87@gmail.com?Subject=Aquí%20el%20asunto%20del%20mail"
               className=" hover:text-purple-950 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125"
