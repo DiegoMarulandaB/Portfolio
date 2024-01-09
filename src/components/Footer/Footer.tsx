aca5d914e599f7e6a4886daf813424b2e8853ebc
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-09 15:51:22 -0500

    style: :recycle: add br on footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,10 +2,10 @@ const Footer = () => {
   return (
     <section id="footer">
       <div className=" mx-auto max-w-full md:max-w-full px-1 sm:px-1 mt-24  bg-slate-100 dark:bg-slate-700">
-        <hr className=" dark:border-slate-500 mb-4"/>
+        <hr className=" dark:border-slate-500 mb-4" />
         <div className="mx-auto p-2 flex flex-col items-center justify-center text-center md:flex-row md:justify-evenly">
           <div className="flex flex-row space-x-2 mt-2 mb-2">
-            &copy; 2023-2024 Diego Marulanda. | All Rights Reserved.
+            &copy; 2023-2024 Diego Marulanda. <br /> All Rights Reserved.
           </div>
         </div>
       </div>
