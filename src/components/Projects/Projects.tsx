1a3473c478b3eb5391edac0bc65fc62678060868
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-10 18:37:42 -0500

    feat: :sparkles: add new description profileGithub

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -75,10 +75,10 @@ const Projects: React.FC = () => {
           <div className="flex flex-wrap rounded-md -mx-4 text-1xl">
             <ProjectCards
               loading="lazy"
-              ImageHref="profileGithub.jpeg"
+              ImageHref="profileGithub.jpg"
               category="All Projects"
               title="My repository"
-              description="Browse my repositories on GitHub, where you will find all my projects from my academic training. And my most relevant personal projects!"
+              description="Browse my repository on GitHub, where you will find all my projects from my academic training at Digital House, Alura, MisionTic. And my most relevant personal projects!"
               button="Read more →"
               buttonHref="https://github.com/DiegoMarulandaB"
               href="https://github.com/DiegoMarulandaB"
