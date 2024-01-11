5e24b438a7aa420a9ab9eb30c987d4d0bb14d564
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-11 16:24:04 -0500

    refactor: :bug: add fixes footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,11 @@
 const Footer = () => {
   return (
     <section id="footer">
-      <div className=" mx-auto max-w-full md:max-w-full px-1 sm:px-1 mt-24  bg-slate-100 dark:bg-slate-700">
-        <hr className="border-slate-500 dark:border-slate-100"/>
+      <div className="mx-auto max-w-full px-4 sm:px-1 md:max-w-full  mt-24  bg-slate-100 dark:bg-slate-700">
+        <hr className="border-slate-500 dark:border-slate-100 mb-1" />
         <div className="mx-auto p-2 flex flex-col items-center justify-center text-center md:flex-row md:justify-between">
-          <div className="flex flex-row space-x-2 mt-1 mb-1 ml-20 mr-20">
-           Diego Marulanda. | Colombia. |  All Rights Reserved.
-          </div>
-          <div className="flex flex-row space-x-2 mt-1 mb-1 mr-20 ml-20">
-            &copy; 2023-2024
-          </div>
+          <div className="flex flex-row space-x-2 mt-1 mb-1 ml-28 mr-28">Diego Marulanda.  👨🏽‍💻  All Rights Reserved</div>
+          <div className="flex flex-row space-x-2 mt-1 mb-1 mr-28 ml-28">&copy; 2023-2024</div>
         </div>
       </div>
     </section>
