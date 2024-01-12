46c9cc85576335a8b5778af26721f5d3e8d71edb
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-12 15:46:44 -0500

    refactor: :bug: add fixes sizes_projects_projectsCards

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,8 +4,6 @@ import React, { useState } from 'react'
 import AnimatedText from '../Animations/AnimatedText/AnimatedText'
 import ProjectCards from '../ProjectsCards/ProjectsCards'
 
-
-
 const Projects: React.FC = () => {
   const [showCard, setShowCard] = useState<string>('all')
 
@@ -35,7 +33,7 @@ const Projects: React.FC = () => {
                 <li className="mb-1">
                   <button
                     onClick={() => handleProject('all')}
-                    className={`inline-block py-3 px-3 text-1xl font-semibold transition md:py-3 lg:px-8 ${
+                    className={`inline-block rounded-md py-3 px-3 text-1xl font-semibold transition md:py-3 lg:px-8 ${
                       showCard === 'all'
                         ? 'activeClasses hover:text-purple-950 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125  mx-2 mr-2'
                         : 'inactiveClasses'
@@ -62,7 +60,7 @@ const Projects: React.FC = () => {
                     onClick={() => handleProject('personal projects')}
                     className={`inline-block py-3 px-3 text-1xl font-semibold transition md:py-3 lg:px-8 ${
                       showCard === 'personal projects'
-                        ? 'activeClasses hover:text-purple-950 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125 mx-2 mr-2'
+                        ? 'activeClasses  hover:text-purple-950 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125 mx-2 mr-2'
                         : 'inactiveClasses'
                     }`}
                   >
@@ -72,15 +70,15 @@ const Projects: React.FC = () => {
               </ul>
             </div>
           </div>
-          <div className="mx-auto mb-[60px] max-w-[810px] text-center">
-            <div className="mx-auto holder w-4/2 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-2">
+          <div className="mx-auto mb-[60px] max-w-[810px]">
+            <div className="flex flex-wrap rounded-md -mx-4 text-1xl">
               <ProjectCards
                 loading="lazy"
                 ImageHref="profileGithub.jpg"
                 category="All Projects"
                 title="My repository"
                 description="Browse my repository on GitHub, where you will find all my projects from my academic training at Digital House, Alura, MisionTic. And my most relevant personal projects!"
-                button="Read More →"
+                button="Read more →"
                 buttonHref="https://github.com/DiegoMarulandaB"
                 href="https://github.com/DiegoMarulandaB"
                 showCard={showCard}
@@ -90,8 +88,8 @@ const Projects: React.FC = () => {
                 ImageHref="digitalBooking.jpeg"
                 category="Academic Projects"
                 title="Digital booking"
-                description="MVP to book activities in several countries.  Technologies: Git, Gitlab, React, Java v. 17, JWT, Gradle,  Spring Boot, MySQL, AWS, Jest.js, Postman, Selenium."
-                button="Read More →"
+                description="MVP to book activities in several countries.  Technologies: Git, Gitlab, Css, React, Java v. 17, JWT, Gradle,  Spring Boot, MySQL, AWS, Jest.js, Postman, Selenium IDE."
+                button="Read more →"
                 buttonHref="https://github.com/DiegoMarulandaB/Digital-booking"
                 href="https://github.com/DiegoMarulandaB/Digital-booking"
                 showCard={showCard}
@@ -102,7 +100,7 @@ const Projects: React.FC = () => {
                 category="Personal Projects"
                 title="Calculator basic"
                 description="Project inspired by the FreeCodeCamp course on basic React. Provides an interactive and functional experience for the user. Technologies: Vite compiler, React library."
-                button="Read More →"
+                button="Read more →"
                 buttonHref="https://github.com/DiegoMarulandaB/interactive-calculator-react"
                 href="https://github.com/DiegoMarulandaB/interactive-calculator-react"
                 showCard={showCard}
@@ -112,8 +110,8 @@ const Projects: React.FC = () => {
                 ImageHref="Final-esp-frontend-2.jpeg"
                 category="Academic Projects"
                 title="The simpsons application"
-                description="Final project of the frontend 2 subject of the frontend specialization in Digital House. Using The Simpsons API. Technologies: Typescript, unit testing, solid principles, validations."
-                button="Read More →"
+                description="Final project of the frontend 2 subject of the frontend specialization in Digital House. Using The Simpsons API. Technologies: Typescript, unit testing, solid principles, clean code, validations."
+                button="Read more →"
                 buttonHref="https://github.com/DiegoMarulandaB/ctd-fe2-proyecto-final-base"
                 href="https://github.com/DiegoMarulandaB/ctd-fe2-proyecto-final-base"
                 showCard={showCard}
@@ -124,7 +122,7 @@ const Projects: React.FC = () => {
                 category="Academic Projects"
                 title="Rick and Morty application"
                 description="Final project of the frontend 1 subject of the frontend specialization in Digital House. Using the Rick and Morty API. Technologies: TypeScript, documentation, Redux, Thunk."
-                button="Read More →"
+                button="Read more →"
                 buttonHref="https://github.com/DiegoMarulandaB/ctd-esp-fe1-final"
                 href="https://github.com/DiegoMarulandaB/ctd-esp-fe1-final"
                 showCard={showCard}
@@ -134,9 +132,9 @@ const Projects: React.FC = () => {
                 ImageHref="frontEnd3.jpeg"
                 category="Academic Projects"
                 title="Health application"
-                description="Final project of the Frontend 3 subject, of the Certified Tech Developer degree. Health application using an API.
+                description="Final project of the Frontend 3 subject, of the Certified Tech Developer degree. Health application using an API, error validations and submit. Technologies: React, Vite..
               Tecnologies: React, Vite."
-                button="Read More →"
+                button="Read more →"
                 buttonHref="https://github.com/DiegoMarulandaB/finalFrontEnd"
                 href="https://github.com/DiegoMarulandaB/finalFrontEnd"
                 showCard={showCard}
@@ -148,7 +146,7 @@ const Projects: React.FC = () => {
                 category="Personal Projects"
                 title="Counter Click"
                 description="Project inspired by the FreeCodeCamp course on basic React. Provides an interactive and functional experience for the user. Technologies: Vite compiler, React library."
-                button="Read More →"
+                button="Read more →"
                 buttonHref="https://github.com/DiegoMarulandaB/click-counter-freecodecamp"
                 href="https://github.com/DiegoMarulandaB/click-counter-freecodecamp"
                 showCard={showCard}
--- src/components/ProjectsCards/ProjectsCards.tsx
+++ src/components/ProjectsCards/ProjectsCards.tsx
@@ -12,39 +12,48 @@ const ProjectCards: React.FC<ProjectCardsProps> = ({
   loading,
   href,
 }) => (
-  <div id="projects">
-    <div className={` ${showCard === 'all' || showCard === category.toLowerCase() ? 'block' : 'hidden'}`}>
-      <Slide offset="-200px 0px -200px 0px">
-        <div className="border border-gray-800 rounded-2xl shadow bg-slate-50 dark:bg-slate-600 dark:border-slate-50 px-1 py-1 mx-2 mr-2 mt-6">
-          <div className="overflow-hidden">
-            <a href={href} target="_blank" rel="noopener noreferrer">
-              <img
-                loading={loading}
-                src={ImageHref}
-                width={690}
-                height={690}
-                alt="projects"
-                className="rounded-t-lg border-slate-800 dark:border-slate-100 transition-transform duration-150 ease-in-out hover:scale-125"
-              />
-            </a>
+  <div
+    id="projects"
+    className={`w-full px-4 md:w-1/2 xl:w-3/3 ${
+      showCard === 'all' || showCard === category.toLowerCase() ? 'block' : 'hidden'
+    }`}
+  >
+    <div className="">
+      <div>
+        <Slide offset="-200px 0px -200px 0px">
+          <div className="flex flex-col items-center text-center border border-gray-800 rounded-2xl shadow bg-slate-50 dark:bg-slate-600 dark:border-slate-50 px-1 py-1 mx-2 mr-2 mt-6">
+            <div className="overflow-hidden">
+              <a href={href} target="_blank" rel="noopener noreferrer">
+                <img
+                  loading={loading}
+                  src={ImageHref}
+                  width={690}
+                  height={690}
+                  alt="projects"
+                  className="rounded-t-lg border-slate-800 dark:border-slate-100 transition-transform duration-150 ease-in-out hover:scale-125"
+                />
+              </a>
+            </div>
+            <div className="mx-auto bg-slate-50  dark:bg-slate-600 rounded-b-lg shadow-xl overflow-hidden">
+              <div className="relative z-10 px-2 py-2 -mt-2 text-center items-center justify-center mx-2 mr-2 w-26 text-1xl">
+                <span className="block mb-2 text-1xl sm:text-1xl md:text-1xl lg:text-1xl xl:text-2xl font-bold">
+                  {category}
+                </span>
+                <h2 className="mb-4 text-1xl text-blue-800 dark:text-amber-200 font-semibold">{title}</h2>
+                <h3 className="mb-4 text-1xl text-slate-800 dark:text-slate-100">{description}</h3>
+                <a
+                  href={buttonHref}
+                  target="_blank"
+                  rel="noopener noreferrer dns-prefetch"
+                  className=" inline-block text-center text-slate-800 hover:text-purple-900 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125 cursor-pointer text-sm font-bold uppercase px-4 py-4 mx-2 mr-2 mt-2 text-1xl"
+                >
+                  {button}
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="bg-slate-50 dark:bg-slate-600 rounded-b-lg shadow-xl overflow-hidden">
-            <span className="block mb-2 text-1xl sm:text-1xl md:text-1xl lg:text-1xl xl:text-2xl font-bold">
-              {category}
-            </span>
-            <h2 className="mb-4 text-1xl font-medium text-blue-800 dark:text-amber-200">{title}</h2>
-            <h3 className="mb-4 text-1xl text-slate-800 dark:text-slate-50">{description}</h3>
-            <a
-              href={buttonHref}
-              target="_blank"
-              rel="noopener noreferrer"
-              className=" inline-block text-center text-slate-800 hover:text-purple-900 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125 cursor-pointer text-sm font-bold px-4 py-4 mx-2 mr-2 mt-2 text-1xl"
-            >
-              {button}
-            </a>
-          </div>
-        </div>
-      </Slide>
+        </Slide>
+      </div>
     </div>
   </div>
 )
