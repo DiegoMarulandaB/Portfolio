e89dbb0b86d692332761422eda04a13486998841
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-13 20:35:49 -0500

    refactor: :bug: add new sizes footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <section id="footer">
-      <div className=" mx-auto max-w-full md:max-w-full px-1 sm:px-1 mt-22  bg-slate-100 dark:bg-slate-700">
+      <div className=" mx-auto max-w-full md:max-w-full px-1 sm:px-1 mt-34  bg-slate-100 dark:bg-slate-700">
         <div className="mx-auto p-2 flex flex-col items-center justify-center text-center md:flex-row md:justify-evenly">
           <div className="flex flex-row space-x-2 mt-1 mb-1">
            Diego Marulanda.  All Rights Reserved.
