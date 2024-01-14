77d181debc3aabb05122948200d44bcc06f758f9
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-14 00:05:15 -0500

    refactor: :sparkles: add about_footer_home_projects_muestra

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,66 +6,63 @@ import MySkills from '../Skills/MySkills'
 const About = () => {
   return (
     <section id="about">
-      <div className="mx-auto lg:px-8 max-w-screen-lg px-4 sm:px-6 py-8">
-        <div className=" mx-auto mr-2 mt-20">
-          <h1>
-            <AnimatedText text="About me"/>
-          </h1>
-        </div>
-        <div className="">
-          <div className="flex flex-col md:flex-row md:p-4 md:text-left mt-2 space-y-10 md:space-x-10 md:space-y-0 items-stretch justify-center align-top">
-            <div className="md:w-3/5">
-              <div className="flex flex-col items-center justify-center">
-                <span className="mx-2 mr-2 mt-4 text-2xl font-bold">About Diego</span>
-                <div className="text-center text-1xl mx-2 mr-2 mt-4 mb-4">
-                  <p className="mb-4 mt-4">
-                    {' '}
-                    My fascination for technology is combined with a deep interest for geopolitics, general culture and
-                    science, which gives me a different perspective in this industry.
-                  </p>
-                </div>
-                <span className="mx-2 mr-2 mt-4 text-blue-800 dark:text-amber-200 text-2xl">Experience</span>
-                <div className="text-center text-1xl mx-2 mr-2 mt-4 mb-4">
-                  <p className="mb-4 mt-4">Of course i have experience in some areas.</p>
-                  <p className="mb-4 mt-4">
-                    But at the moment i have not had the opportunity to enter the technology sector. Therefore, i will
-                    get my experience through the different projects that i carry out.
-                  </p>
-                </div>
-                <span className="mx-2 mr-2 mt-4 text-blue-800 dark:text-amber-200 text-2xl">Studies</span>
-                <div className="text-center text-1xl mx-2 mr-2 mt-4 mb-4">
-                  <p className="mb-4 mt-4">
-                    <span>Digital House 2023 - 2024</span>
-                  </p>
-                  <p>Frontend specialization at Digital House co-created with Mercado Libre and Globant.</p>
-                  <p className="mb-4 mt-4">
-                    <span>Digital House 2021 - 2023</span>
-                  </p>
-                  <p>Certified Tech Developer career at Digital House co-created with Mercado Libre and Globant.</p>
-                  <p className="mb-4 mt-4">
-                    <span>Alura Latam 2022 - 2022</span>
-                  </p>
-                  <p>Oracle Next Education Program Certification.</p>
-                  <p className="mb-4 mt-4">
-                    <span>MisionTic 2021 - 2021</span>
-                  </p>
-                  <p>
-                    {' '}
-                    Program of the Government of Colombia together with the MinTic, and the Pontificia Bolivariana
-                    university to train programmers.
-                  </p>
-                </div>
-                <span className="mx-2 mr-2 mt-4 text-blue-800 dark:text-amber-200 text-2xl">Hobbies</span>
-                <div className="text-center text-1xl mx-2 mr-2 mt-4 mb-4">
-                  <p className="mb-4 mt-4">
-                    My hobbies are listening to rock music, video games, books on different themes.
-                  </p>
-                  <p className="mb-4 mt-4"> My favorite writer is Brandon Sanderson.</p>
-                </div>
+      <h1>
+        <AnimatedText text="About me" className=" text-center justify-center mx-2 mr-2 mt-24" />
+      </h1>
+
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="flex flex-col md:flex-row md:p-4 md:text-left mt-2 space-y-10 md:space-x-10 md:space-y-0 items-stretch justify-center align-top">
+          <div className="md:w-3/5">
+            <div className="flex flex-col items-center justify-center">
+              <span className="mx-2 mr-2 mt-4 text-2xl font-bold">About Diego</span>
+              <div className="text-center text-1xl mx-2 mr-2 mt-4 mb-4">
+                <p className="mb-4 mt-4">
+                  {' '}
+                  My fascination for technology is combined with a deep interest for geopolitics, general culture and
+                  science, which gives me a different perspective in this industry.
+                </p>
+              </div>
+              <span className="mx-2 mr-2 mt-4 text-blue-800 dark:text-amber-200 text-2xl">Experience</span>
+              <div className="text-center text-1xl mx-2 mr-2 mt-4 mb-4">
+                <p className="mb-4 mt-4">Of course i have experience in some areas.</p>
+                <p className="mb-4 mt-4">
+                  But at the moment i have not had the opportunity to enter the technology sector. Therefore, i will get
+                  my experience through the different projects that i carry out.
+                </p>
+              </div>
+              <span className="mx-2 mr-2 mt-4 text-blue-800 dark:text-amber-200 text-2xl">Studies</span>
+              <div className="text-center text-1xl mx-2 mr-2 mt-4 mb-4">
+                <p className="mb-4 mt-4">
+                  <span>Digital House 2023 - 2024</span>
+                </p>
+                <p>Frontend specialization at Digital House co-created with Mercado Libre and Globant.</p>
+                <p className="mb-4 mt-4">
+                  <span>Digital House 2021 - 2023</span>
+                </p>
+                <p>Certified Tech Developer career at Digital House co-created with Mercado Libre and Globant.</p>
+                <p className="mb-4 mt-4">
+                  <span>Alura Latam 2022 - 2022</span>
+                </p>
+                <p>Oracle Next Education Program Certification.</p>
+                <p className="mb-4 mt-4">
+                  <span>MisionTic 2021 - 2021</span>
+                </p>
+                <p>
+                  {' '}
+                  Program of the Government of Colombia together with the MinTic, and the Pontificia Bolivariana
+                  university to train programmers.
+                </p>
+              </div>
+              <span className="mx-2 mr-2 mt-4 text-blue-800 dark:text-amber-200 text-2xl">Hobbies</span>
+              <div className="text-center text-1xl mx-2 mr-2 mt-4 mb-4">
+                <p className="mb-4 mt-4">
+                  My hobbies are listening to rock music, video games, books on different themes.
+                </p>
+                <p className="mb-4 mt-4"> My favorite writer is Brandon Sanderson.</p>
               </div>
             </div>
-            <MySkills />
           </div>
+          <MySkills />
         </div>
       </div>
     </section>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -13,145 +13,143 @@ const Projects: React.FC = () => {
 
   return (
     <section id="projects">
-      <div className="min-h-screen flex items-center justify-center py-8">
-        <div className="min-h-screen flex items-center justify-center">
-          <div className="flex flex-col text-center items-center justify-center mx-4 mr-4">
-            <div className="flex flex-wrap -mx-4">
-              <div className="mx-auto mb-[60px] max-w-[510px] text-center">
-                <h1>
-                  <AnimatedText text="Projects" className="mx-auto mr-2 mt-20" />
-                </h1>
-                <p className="text-1xl text-body-color text-center mx-2 mr-2 mt-10">
-                  In this section, you can find samples of my projects, i hope you enjoy exploring my work as much as i
-                  enjoy creating it.
-                </p>
-              </div>
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="flex flex-col text-center items-center justify-center mx-4 mr-4">
+          <div className="flex flex-wrap -mx-4">
+            <div className="mx-auto mb-[60px] max-w-[510px] text-center">
+              <h1>
+                <AnimatedText text="Projects" className="text-center mx-2 mr-2 mt-24" />
+              </h1>
+              <p className="text-1xl text-body-color text-center mx-2 mr-2 mt-8">
+                In this section, you can find samples of my projects, i hope you enjoy exploring my work as much as i
+                enjoy creating it.
+              </p>
             </div>
+          </div>
 
-            <div className="w-full flex flex-wrap justify-center -mx-4">
-              <div className="w-full px-4">
-                <ul className="flex flex-wrap justify-center mb-12 space-x-1">
-                  <li className="mb-1">
-                    <button
-                      onClick={() => handleProject('all')}
-                      className={`inline-block rounded-md py-3 px-3 text-1xl font-semibold transition md:py-3 lg:px-8 ${
-                        showCard === 'all'
-                          ? 'activeClasses hover:text-purple-950 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125  mx-2 mr-2'
-                          : 'inactiveClasses'
-                      }`}
-                    >
-                      All Projects
-                    </button>
-                  </li>
+          <div className="w-full flex flex-wrap justify-center -mx-4">
+            <div className="w-full px-4">
+              <ul className="flex flex-wrap justify-center mb-12 space-x-1">
+                <li className="mb-1">
+                  <button
+                    onClick={() => handleProject('all')}
+                    className={`inline-block rounded-md py-3 px-3 text-1xl font-semibold transition md:py-3 lg:px-8 ${
+                      showCard === 'all'
+                        ? 'activeClasses hover:text-purple-950 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125  mx-2 mr-2'
+                        : 'inactiveClasses'
+                    }`}
+                  >
+                    All Projects
+                  </button>
+                </li>
 
-                  <li className="mb-1">
-                    <button
-                      onClick={() => handleProject('academic projects')}
-                      className={`inline-block py-3 px-3 text-1xl  font-semibold transition md:py-3 lg:px-8 ${
-                        showCard === 'academic projects'
-                          ? 'activeClasses hover:text-purple-950 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125  mx-2 mr-2'
-                          : 'inactiveClasses'
-                      }`}
-                    >
-                      Academic Projects
-                    </button>
-                  </li>
-                  <li className="mb-1">
-                    <button
-                      onClick={() => handleProject('personal projects')}
-                      className={`inline-block py-3 px-3 text-1xl font-semibold transition md:py-3 lg:px-8 ${
-                        showCard === 'personal projects'
-                          ? 'activeClasses  hover:text-purple-950 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125 mx-2 mr-2'
-                          : 'inactiveClasses'
-                      }`}
-                    >
-                      Personal Projects
-                    </button>
-                  </li>
-                </ul>
-              </div>
+                <li className="mb-1">
+                  <button
+                    onClick={() => handleProject('academic projects')}
+                    className={`inline-block py-3 px-3 text-1xl  font-semibold transition md:py-3 lg:px-8 ${
+                      showCard === 'academic projects'
+                        ? 'activeClasses hover:text-purple-950 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125  mx-2 mr-2'
+                        : 'inactiveClasses'
+                    }`}
+                  >
+                    Academic Projects
+                  </button>
+                </li>
+                <li className="mb-1">
+                  <button
+                    onClick={() => handleProject('personal projects')}
+                    className={`inline-block py-3 px-3 text-1xl font-semibold transition md:py-3 lg:px-8 ${
+                      showCard === 'personal projects'
+                        ? 'activeClasses  hover:text-purple-950 dark:text-slate-100 dark:hover:text-green-300 transition-transform duration-150 ease-in-out hover:scale-125 mx-2 mr-2'
+                        : 'inactiveClasses'
+                    }`}
+                  >
+                    Personal Projects
+                  </button>
+                </li>
+              </ul>
             </div>
-            <div className="mx-auto mb-[60px] max-w-[810px]">
-              <div className="flex flex-wrap -mx-4 text-1xl">
-                <ProjectCards
-                  loading="lazy"
-                  ImageHref="profileGithub.jpg"
-                  category="All Projects"
-                  title="My repository"
-                  description="Browse my repository on GitHub, where you will find all my projects from my academic training at Digital House, Alura, MisionTic. And my most relevant personal projects!"
-                  button="Read More →"
-                  buttonHref="https://github.com/DiegoMarulandaB"
-                  href="https://github.com/DiegoMarulandaB"
-                  showCard={showCard}
-                />
-                <ProjectCards
-                  loading="lazy"
-                  ImageHref="digitalBooking.jpeg"
-                  category="Academic Projects"
-                  title="Digital booking"
-                  description="MVP to book activities in several countries.  Technologies: Git, Gitlab, Css, React, Java version 17, JWT, Gradle,  Spring Boot, MySQL, AWS, Jest.js, Postman, Selenium IDE."
-                  button="Read More →"
-                  buttonHref="https://github.com/DiegoMarulandaB/Digital-booking"
-                  href="https://github.com/DiegoMarulandaB/Digital-booking"
-                  showCard={showCard}
-                />
-                <ProjectCards
-                  loading="lazy"
-                  ImageHref="calculator.jpeg"
-                  category="Personal Projects"
-                  title="Calculator basic"
-                  description="Project inspired by the FreeCodeCamp course on basic React. Provides an interactive and functional experience for the user. Technologies: Vite compiler, React library."
-                  button="Read More →"
-                  buttonHref="https://github.com/DiegoMarulandaB/interactive-calculator-react"
-                  href="https://github.com/DiegoMarulandaB/interactive-calculator-react"
-                  showCard={showCard}
-                />
-                <ProjectCards
-                  loading="lazy"
-                  ImageHref="Final-esp-frontend-2.jpeg"
-                  category="Academic Projects"
-                  title="The simpsons application"
-                  description="Final project of the frontend 2 subject of the frontend specialization in Digital House. Using The Simpsons API. Technologies: Typescript, unit testing, solid principles, clean code, validations."
-                  button="Read More →"
-                  buttonHref="https://github.com/DiegoMarulandaB/ctd-fe2-proyecto-final-base"
-                  href="https://github.com/DiegoMarulandaB/ctd-fe2-proyecto-final-base"
-                  showCard={showCard}
-                />
-                <ProjectCards
-                  loading="lazy"
-                  ImageHref="Final-esp-frontend.png"
-                  category="Academic Projects"
-                  title="Rick and Morty application"
-                  description="Final project of the frontend subject 1 of the frontend specialization in Digital House. Using the Rick and Morty API with documentation. Technologies: TypeScript, Redux, Thunk."
-                  button="Read More →"
-                  buttonHref="https://github.com/DiegoMarulandaB/ctd-esp-fe1-final"
-                  href="https://github.com/DiegoMarulandaB/ctd-esp-fe1-final"
-                  showCard={showCard}
-                />
-                <ProjectCards
-                  loading="lazy"
-                  ImageHref="frontEnd3.jpeg"
-                  category="Academic Projects"
-                  title="Health application"
-                  description="Final project of the Frontend 3 subject, of the Certified Tech Developer degree. Health application using an API, error validations and submit. Technologies: React, Vite."
-                  button="Read More →"
-                  buttonHref="https://github.com/DiegoMarulandaB/finalFrontEnd"
-                  href="https://github.com/DiegoMarulandaB/finalFrontEnd"
-                  showCard={showCard}
-                />
+          </div>
+          <div className="mx-auto mb-[60px] max-w-[810px]">
+            <div className="flex flex-wrap -mx-4 text-1xl">
+              <ProjectCards
+                loading="lazy"
+                ImageHref="profileGithub.jpg"
+                category="All Projects"
+                title="My repository"
+                description="Browse my repository on GitHub, where you will find all my projects from my academic training at Digital House, Alura, MisionTic. And my most relevant personal projects!"
+                button="Read More →"
+                buttonHref="https://github.com/DiegoMarulandaB"
+                href="https://github.com/DiegoMarulandaB"
+                showCard={showCard}
+              />
+              <ProjectCards
+                loading="lazy"
+                ImageHref="digitalBooking.jpeg"
+                category="Academic Projects"
+                title="Digital booking"
+                description="MVP to book activities in several countries.  Technologies: Git, Gitlab, Css, React, Java version 17, JWT, Gradle,  Spring Boot, MySQL, AWS, Jest.js, Postman, Selenium IDE."
+                button="Read More →"
+                buttonHref="https://github.com/DiegoMarulandaB/Digital-booking"
+                href="https://github.com/DiegoMarulandaB/Digital-booking"
+                showCard={showCard}
+              />
+              <ProjectCards
+                loading="lazy"
+                ImageHref="calculator.jpeg"
+                category="Personal Projects"
+                title="Calculator basic"
+                description="Project inspired by the FreeCodeCamp course on basic React. Provides an interactive and functional experience for the user. Technologies: Vite compiler, React library."
+                button="Read More →"
+                buttonHref="https://github.com/DiegoMarulandaB/interactive-calculator-react"
+                href="https://github.com/DiegoMarulandaB/interactive-calculator-react"
+                showCard={showCard}
+              />
+              <ProjectCards
+                loading="lazy"
+                ImageHref="Final-esp-frontend-2.jpeg"
+                category="Academic Projects"
+                title="The simpsons application"
+                description="Final project of the frontend 2 subject of the frontend specialization in Digital House. Using The Simpsons API. Technologies: Typescript, unit testing, solid principles, clean code, validations."
+                button="Read More →"
+                buttonHref="https://github.com/DiegoMarulandaB/ctd-fe2-proyecto-final-base"
+                href="https://github.com/DiegoMarulandaB/ctd-fe2-proyecto-final-base"
+                showCard={showCard}
+              />
+              <ProjectCards
+                loading="lazy"
+                ImageHref="Final-esp-frontend.png"
+                category="Academic Projects"
+                title="Rick and Morty application"
+                description="Final project of the frontend subject 1 of the frontend specialization in Digital House. Using the Rick and Morty API with documentation. Technologies: TypeScript, Redux, Thunk."
+                button="Read More →"
+                buttonHref="https://github.com/DiegoMarulandaB/ctd-esp-fe1-final"
+                href="https://github.com/DiegoMarulandaB/ctd-esp-fe1-final"
+                showCard={showCard}
+              />
+              <ProjectCards
+                loading="lazy"
+                ImageHref="frontEnd3.jpeg"
+                category="Academic Projects"
+                title="Health application"
+                description="Final project of the Frontend 3 subject, of the Certified Tech Developer degree. Health application using an API, error validations and submit. Technologies: React, Vite."
+                button="Read More →"
+                buttonHref="https://github.com/DiegoMarulandaB/finalFrontEnd"
+                href="https://github.com/DiegoMarulandaB/finalFrontEnd"
+                showCard={showCard}
+              />
 
-                <ProjectCards
-                  loading="lazy"
-                  ImageHref="counterclick.jpeg"
-                  category="Personal Projects"
-                  title="Counter Click"
-                  description="Project inspired by the FreeCodeCamp course on basic React. Provides an interactive and functional experience for the user. Technologies: Vite compiler, React library."
-                  button="Read More →"
-                  buttonHref="https://github.com/DiegoMarulandaB/click-counter-freecodecamp"
-                  href="https://github.com/DiegoMarulandaB/click-counter-freecodecamp"
-                  showCard={showCard}
-                />
-              </div>
+              <ProjectCards
+                loading="lazy"
+                ImageHref="counterclick.jpeg"
+                category="Personal Projects"
+                title="Counter Click"
+                description="Project inspired by the FreeCodeCamp course on basic React. Provides an interactive and functional experience for the user. Technologies: Vite compiler, React library."
+                button="Read More →"
+                buttonHref="https://github.com/DiegoMarulandaB/click-counter-freecodecamp"
+                href="https://github.com/DiegoMarulandaB/click-counter-freecodecamp"
+                showCard={showCard}
+              />
             </div>
           </div>
         </div>
