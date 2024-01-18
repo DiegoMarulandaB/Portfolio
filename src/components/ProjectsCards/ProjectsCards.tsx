a99ea99e8bf04cb71f2207b1cb6497007d2dd5f1
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-18 16:39:15 -0500

    refactor: :sparkles: add new projectsCards

--- src/components/ProjectsCards/ProjectsCards.tsx
+++ src/components/ProjectsCards/ProjectsCards.tsx
@@ -20,7 +20,7 @@ const ProjectCards: React.FC<ProjectCardsProps> = ({
   >
     <div>
       <Slide offset="-200px 0px -200px 0px">
-        <div className="flex flex-col items-center text-center rounded-2xl shadow bg-slate-50 dark:bg-slate-600 dark:border-slate-50 px-2 py-2 mx-2 mr-2 mt-6 shadow-blue-700/60 dark:shadow-lg dark:shadow-slate-50/40">
+        <div className="flex flex-col items-center text-center rounded-2xl shadow bg-slate-50 dark:bg-slate-600 dark:border-slate-50 px-4 py-4 mx-2 mr-2 mt-6 shadow-blue-700/60 dark:shadow-lg dark:shadow-slate-50/40">
           <div className="overflow-hidden">
             <a href={href} target="_blank" rel="noopener noreferrer">
               <img
@@ -29,11 +29,11 @@ const ProjectCards: React.FC<ProjectCardsProps> = ({
                 width={690}
                 height={690}
                 alt="projects"
-                className="rounded-t-lg border-solid border-2 border-slate-500 dark:border-slate-50 transition-transform duration-150 ease-in-out hover:scale-125"
+                className="rounded-lg border-solid border-2 border-slate-500 dark:border-slate-50 transition-transform duration-150 ease-in-out hover:scale-125"
               />
             </a>
           </div>
-          <div className="mx-auto overflow-hidden rounded-b-lg">
+          <div className="mx-auto overflow-hidden">
             <div className="relative z-10 px-2 py-2 -mt-2 text-center items-center justify-center mx-2 mr-2 w-26 text-1xl">
               <span className="block mb-2 text-1xl sm:text-1xl md:text-1xl lg:text-1xl xl:text-2xl font-bold">
                 {category}
