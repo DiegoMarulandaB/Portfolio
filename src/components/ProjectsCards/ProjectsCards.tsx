1b73943c8d4d4fd4d4aeba0b8fb70797afef8fe8
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-19 09:10:23 -0500

    refactor: :sparkles: add new style projectsCards

--- src/components/ProjectsCards/ProjectsCards.tsx
+++ src/components/ProjectsCards/ProjectsCards.tsx
@@ -20,7 +20,7 @@ const ProjectCards: React.FC<ProjectCardsProps> = ({
   >
     <div>
       <Slide offset="-200px 0px -200px 0px">
-        <div className="flex flex-col items-center text-center rounded-2xl shadow bg-slate-50 dark:bg-slate-600 dark:border-slate-50 mx-2 mr-2 mt-6">
+        <div className="flex flex-col items-center text-center rounded-lg  bg-slate-50 dark:bg-slate-600 shadow-lg shadow-blue-500/40 dark:shadow-slate-50/40  mx-2 mr-2 mt-6">
           <div className="overflow-hidden">
             <a href={href} target="_blank" rel="noopener noreferrer">
               <img
