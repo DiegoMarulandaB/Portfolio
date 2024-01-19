338c4b6d2b54f50668aeb361b6e5af25e500c6d6
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-19 16:09:11 -0500

    refactor: :fire: delete animatedText on not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,11 @@
 'use client'
-import AnimatedText from '../components/Animations/AnimatedText/AnimatedText'
 function NotFound() {
   return (
     <div className=" bg-slate-100 dark:bg-slate-700 py-4 px-4 sm:px-6 lg:px-8">
       <div className="min-h-screen flex items-center justify-center">
         <div className="flex flex-col items-center text-center text-white bg-gradient-to-r bg-white dark:bg-slate-600 opacity-90 rounded-3xl px-20 py-24 mx-2 mr-2 mt-10 w-26 text-2xl  font-bold ">
-          <h1 className="text-center text-8xl text-neutral-800 dark:text-slate-100  font-bold mb-4">
-            <AnimatedText text="Error 404" className="mx-2 mr-2 mb-2 mt-6 " />
+          <h1 className="text-center text-8xl text-neutral-800 dark:text-slate-100 mx-2 mr-2 mb-2 mt-4 font-bold">
+            Error 404
           </h1>
           <p className="text-center text-4xl text-blue-800 dark:text-green-400 mb-6">
             Oops! The page you are looking for doesn't exist.
