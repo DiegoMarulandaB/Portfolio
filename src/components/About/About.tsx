6e4655ee462a03c60e7d72555f8cd9418d56432d
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-19 22:58:13 -0500

    refactor: :bug: add fixes sizes_about_experience_projects

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,12 +5,12 @@ import MySkills from '../Skills/MySkills'
 const About = () => {
   return (
     <section id="about">
-      <h1 className="text-center justify-center mx-2 mr-2 mt-24 pt-24 font-extrabold capitalize text-4xl sm:text-7xl md:text-6xl lg:text-6xl xl:text-7xl">
-        About me
-      </h1>
+      <div className=" text-center justify-center mx-2 mr-2 mt-24 font-extrabold capitalize text-4xl sm:text-7xl md:text-6xl lg:text-6xl xl:text-7xl">
+        <h1>About me</h1>
+      </div>
       <div className="min-h-screen flex items-center justify-center">
         <div className="flex flex-col md:flex-row md:p-4 md:text-left mt-2 space-y-10 md:space-x-10 md:space-y-0 items-stretch justify-center align-top">
-          <div className="md:w-2/3">
+          <div className="md:w-3/5">
             <div className="flex flex-col items-center justify-center">
               <span className="text-center justify-center mx-2 mr-2 mt-4 text-2xl dark:text-white font-bold">
                 About Diego
@@ -22,6 +22,16 @@ const About = () => {
                   science, which gives me a different perspective in this industry.
                 </p>
               </div>
+              {/* <span className="mx-2 mr-2 mt-4 text-blue-800 dark:text-amber-200 text-2xl">Experience</span>
+              <div className="text-center text-1xl dark:text-white mx-2 mr-2 mt-4 mb-4">
+                <p className="mb-4 mt-4">
+                  I will acquire my experience through various projects, both academic and personal, my focus is on
+                  innovation and constant growth.
+                </p>
+                <p className="mb-4 mt-4">
+                  My background reflects my commitment to excellence and creative problem solving.
+                </p>
+              </div> */}
               <span className="mx-2 mr-2 mt-4 text-blue-800 dark:text-amber-200 text-2xl">Studies</span>
               <div className="text-center text-1xl dark:text-white mx-2 mr-2 mt-4 mb-4">
                 <p className="mb-4 mt-4">
