ebe7aa8aaf97619453bd6bd14e4af4b39fd35fa4
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-20 10:02:41 -0500

    refactor: :bug: add fixes sizes-footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,11 +4,11 @@ const Footer = () => {
       <div className="mx-auto max-w-full md:max-w-full sm:px-1 bg-slate-100 dark:bg-slate-700 mt-6">
         <hr className="dark:border-slate-500 mb-4" />
         <div className="mx-auto flex flex-col items-center justify-center text-center md:flex-row md:justify-center">
-          <div className="flex flex-row space-x-2 mt-2 mb-2">
+          <div className="flex flex-row space-x-2 mt-1 mb-1">
             <span className="text-slate-800 dark:text-white">&copy;&nbsp;2023-2024</span>
             <span className="text-blue-800 dark:text-amber-200">Diego Marulanda.</span>
           </div>
-          <div className="flex flex-row space-x-2 mt-2 mb-2">
+          <div className="flex flex-row space-x-2 mt-1 mb-1">
             <span className="text-slate-800 dark:text-white">&nbsp; All Rights Reserved.</span>
           </div>
         </div>
