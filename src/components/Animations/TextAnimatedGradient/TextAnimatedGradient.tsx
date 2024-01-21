fe197c0d5daa9bf8a722c242577d236555490e54
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-21 18:20:32 -0500

    refactor: :bug: add fixes motion h1

--- src/components/Animations/TextAnimatedGradient/TextAnimatedGradient.tsx
+++ src/components/Animations/TextAnimatedGradient/TextAnimatedGradient.tsx
@@ -22,7 +22,7 @@ const TextAnimatedGradient = () => {
       animate="visible"
       variants={textVariants}
       className="inline-flex animate-text-gradient bg-gradient-to-r
-        from-[#60a5fa] via-[#2563eb] to-[#93c5fd] dark:from-[#fde68a] dark:via-[#f8fafc] dark:to-[#fde68a] bg-[200%_auto] bg-clip-text text-transparent mx-2 mr-2 mt-24 font-extrabold capitalize text-4xl sm:text-6xl md:text-4xl lg:text-6xl xl:text-7xl"
+        from-[#60a5fa] via-[#2563eb] to-[#93c5fd] dark:from-[#fde68a] dark:via-[#f8fafc] dark:to-[#fde68a] bg-[200%_auto] bg-clip-text text-transparent mx-2 mr-2 mt-24 font-extrabold capitalize text-4xl sm:text-6xl md:text-4xl lg:text-5xl xl:text-7xl"
     >
       <motion.h1>Dive into the world of code and creativity!</motion.h1>
     </motion.div>
