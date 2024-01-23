54008929575d49818c916830762af05f487595f5
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-22 21:34:50 -0500

    refactor: :bug: add new sizes projectsCards

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -16,11 +16,10 @@ const Projects: React.FC = () => {
         <div className=" text-start justify-start ml-2 mx-1 mr-1 font-extrabold capitalize text-4xl sm:text-6xl md:text-4xl lg:text-4xl xl:text-7xl">
           <h1>Projects</h1>
         </div>
-        <div className="min-h-screen flex items-start justify-start ml-2 mx-1 mr-1">
+        <div className="min-h-screen flex items-start justify-start ml-2 mx-2 mr-2">
           <div className="justify-start">
             <div className="flex flex-wrap -mx-4">
-              <div className="mx-auto mb-[56px] max-w-[510px]">
-              </div>
+              <div className="mx-auto mb-[17px] max-w-[510px]"></div>
             </div>
             <div className="w-full flex flex-wrap justify-start -mx-10">
               <ul className="flex flex-wrap justify-center mb-12 space-x-1">
@@ -29,7 +28,7 @@ const Projects: React.FC = () => {
                     onClick={() => handleProject('all')}
                     className={`inline-block py-3 px-3 text-1xl font-semibold transition md:py-3 lg:px-8 ${
                       showCard === 'all'
-                        ? 'activeClasses text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100  transition-transform duration-150 ease-in-out hover:scale-125  mx-2 mr-2'
+                        ? 'activeClasses text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 mx-2 mr-2'
                         : 'inactiveClasses'
                     }`}
                   >
@@ -42,7 +41,7 @@ const Projects: React.FC = () => {
                     onClick={() => handleProject('academic projects')}
                     className={`inline-block py-3 px-3 text-1xl  font-semibold transition md:py-3 lg:px-8 ${
                       showCard === 'academic projects'
-                        ? 'activeClassestext-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100  transition-transform duration-150 ease-in-out hover:scale-125  mx-2 mr-2'
+                        ? 'activeClassestext-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100  mx-2 mr-2'
                         : 'inactiveClasses'
                     }`}
                   >
@@ -54,7 +53,7 @@ const Projects: React.FC = () => {
                     onClick={() => handleProject('personal projects')}
                     className={`inline-block py-3 px-3 text-1xl font-semibold transition md:py-3 lg:px-8 ${
                       showCard === 'personal projects'
-                        ? 'activeClasses text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 transition-transform duration-150 ease-in-out hover:scale-125 mx-2 mr-2'
+                        ? 'activeClasses text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100  mx-2 mr-2'
                         : 'inactiveClasses'
                     }`}
                   >
@@ -63,14 +62,14 @@ const Projects: React.FC = () => {
                 </li>
               </ul>
             </div>
-            <div className="mx-auto mb-[60px] max-w-[810px]">
+            <div className="mx-auto mb-[60px] max-w-[710px]">
               <div className="flex flex-wrap -mx-4 text-1xl">
                 <ProjectCards
                   loading="lazy"
                   ImageHref="profileGithub.webp"
                   category="All Projects"
                   title="My repository on Github"
-                  description="Browse all my GitHub repositories, where you can discover a variety of projects from my academic training in programming. And my most relevant personal projects!"
+                  description="Browse all my repositories on GitHub, where you can discover a variety of projects from my academic training at Digital House, Alura Latam, MisionTic among others. And my most relevant personal projects!"
                   button="Github →"
                   buttonHref="https://github.com/DiegoMarulandaB"
                   href="https://github.com/DiegoMarulandaB"
@@ -81,7 +80,7 @@ const Projects: React.FC = () => {
                   ImageHref="digitalBooking.webp"
                   category="Academic Projects"
                   title="Digital booking"
-                  description="MVP to book tours and different activities in various countries. Technologies: Git, Gitlab, CSS, React, Java version 17, JWT, Gradle, Spring Boot, MySQL, AWS, Jest.JS, Postman, Selenium IDE."
+                  description="MVP to book tours and different activities in various countries. Technologies: Git, Gitlab, CSS, React, Java v. 17, JWT, Gradle, Spring Boot, MySQL,   infrastructure AWS, Jest.JS, Postman, Selenium IDE."
                   button="Github →"
                   buttonHref="https://github.com/DiegoMarulandaB/Digital-booking"
                   href="https://github.com/DiegoMarulandaB/Digital-booking"
@@ -92,7 +91,7 @@ const Projects: React.FC = () => {
                   ImageHref="calculator.webp"
                   category="Personal Projects"
                   title="Calculator basic"
-                  description="Project inspired by the FreeCodeCamp course on basic React. Provides an interactive and functional experience for the user. Technologies: Vite compiler, React library."
+                  description="Project inspired by FreeCodeCamp's YouTube course on basic React. It provides an interactive experience, basic and functional design for the user. Technologies: Vite compiler, React library."
                   button="Github →"
                   buttonHref="https://github.com/DiegoMarulandaB/interactive-calculator-react"
                   href="https://github.com/DiegoMarulandaB/interactive-calculator-react"
@@ -137,7 +136,7 @@ const Projects: React.FC = () => {
                   ImageHref="counterclick.webp"
                   category="Personal Projects"
                   title="Counter Click"
-                  description="Project inspired by the FreeCodeCamp course on basic React. Provides an interactive and functional experience for the user. Technologies: Vite compiler, React library."
+                  description="Project inspired by FreeCodeCamp's YouTube course on basic React. It provides an interactive experience, basic and functional design for the user. Technologies: Vite compiler, React library."
                   button="Github →"
                   buttonHref="https://github.com/DiegoMarulandaB/click-counter-freecodecamp"
                   href="https://github.com/DiegoMarulandaB/click-counter-freecodecamp"
