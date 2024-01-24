cf236339af360e8d61e76a952361d565be6949ee
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-23 19:20:01 -0500

    refactor: :sparkles: add currentYear

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,16 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
-    <section id="footer">
-      <div className="mx-auto max-w-full md:max-w-full sm:px-1 bg-slate-100 dark:bg-slate-700 mt-10">
+    <section id="footer" className="mt-10">
+      <div className="mx-auto max-w-full md:max-w-full sm:px-1 bg-slate-100 dark:bg-slate-700">
         <hr className="dark:border-slate-500 mb-4" />
         <div className="mx-auto flex flex-col items-center justify-center text-center md:flex-row md:justify-center">
-          <div className="flex flex-row space-x-2 mt-1 mb-1">
-            <span className="text-slate-800 dark:text-white">&copy;&nbsp;2023-2024</span>
+          <div className="flex flex-row space-x-2 mt-2 mb-2">
+            <span className="text-slate-800 dark:text-white">&copy;&nbsp;2023-{currentYear}</span>
             <span className="text-blue-800 dark:text-amber-200">Diego Marulanda.</span>
           </div>
-          <div className="flex flex-row space-x-2 mt-1 mb-1">
+          <div className="flex flex-row space-x-2 mt-2 mb-2">
             <span className="text-slate-800 dark:text-white">&nbsp; All Rights Reserved.</span>
           </div>
         </div>
