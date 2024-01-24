552913df0c7bd3216190e31523359bd642238f0c
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-24 16:28:32 -0500

    refactor: :sparkles: ad new sizes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,12 +7,12 @@ const Footer = () => {
         <div className="mb-2">
           <hr />
         </div>
-        <div className="mx-auto flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center space-x-2 ml-6 mr-6">
-          <div className="flex flex-row space-x-2 mt-1 mb-1">
+        <div className="mx-auto flex flex-col md:flex-row justify-start md:items-center md:text-center md:justify-center space-x-2 ml-6 mr-6">
+          <div className="flex flex-row space-x-2 mt-2 mb-2">
             <span className="text-slate-800 dark:text-white">&copy;&nbsp;2023-{currentYear}</span>
             <span className="text-blue-800 dark:text-amber-200">Diego Marulanda.</span>
           </div>
-          <div className="flex flex-row space-x-2 mt-1 mb-1">
+          <div className="flex flex-row space-x-2 mt-2 mb-2">
             <span className="text-slate-800 dark:text-white">All Rights Reserved.</span>
           </div>
         </div>
