3deff3404011cc655f8af65680aefe942eae275e
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-24 20:13:03 -0500

    refactor: :sparkles: aad new description hobbies

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -58,13 +58,14 @@ const About = () => {
                   <FaBook className="text-blue-800 dark:text-amber-200 text-2xl mb-3 mt-1" size={24} />
                   <span className="text-blue-800 dark:text-amber-200 text-2xl ml-2">Hobbies</span>
                 </div>
-                <div className="[&>p]:mb-6 mr-1 [&>p]:mt-7 text-start">
+                <div className="[&>p]:mb-6 mr-1 [&>p]:mt-6 text-start">
                   <p>My hobbies:</p>
-                  <span className="block mt-6">listening to rock music.</span>
-                  <span className="block mt-6">video games.</span>
-                  <span className="block mt-6">books on different themes.</span>
+                  <p className="">
+                    I am a rock fan, I enjoy immersing myself in the exciting worlds of video games and explore a wide
+                    variety of topics through books or articles.
+                  </p>
                 </div>
-                <div className="[&>p]:mb-6 mr-1 [&>p]:mt-6 ">
+                <div className=" mr-1 [&>p]:mt-1 ">
                   <p> My favorite writer:</p>
                   <span className="block mt-6 mb-20">Brandon Sanderson.</span>
                 </div>
