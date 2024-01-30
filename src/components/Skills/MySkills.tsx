52488be65b0a4b01872704064c58fdb194ff8076
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-30 16:41:17 -0500

    refactor: :bug: add fixes sizes

--- src/components/Skills/MySkills.tsx
+++ src/components/Skills/MySkills.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 const MySkills = () => {
   return (
-    <div className="md:w-1/2">
-      <div className=" flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center mr-4 font-extrabold capitalize text-4xl sm:text-6xl md:text-4xl lg:text-4xl xl:text-7xl mt-[-52px]">
+    <div className="md:w-1/2 mt-[-52px]">
+      <div className=" flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center mr-4 font-extrabold capitalize text-4xl sm:text-6xl md:text-4xl lg:text-4xl xl:text-7xl">
         <h2>My Skills</h2>
       </div>
       <div className="flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center text-2xl mr-2 mt-4 py-2  text-blue-800 dark:text-amber-200">
