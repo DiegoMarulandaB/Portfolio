98deda63c63f04ecf8adbaa56a765c0bf46c8cea
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-01-30 15:09:11 -0500

    refactor: :sparkles: add center navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -22,11 +22,11 @@ const Navigation = () => {
 
   return (
     <header className="w-full mx-auto px-6 sm:px-20 opacity-90 fixed top-0 z-50 shadow bg-slate-100 dark:bg-slate-700 dark:border-slate-500">
-      <div className="justify-evenly md:items-center md:flex">
+      <div className="justify-center md:items-center md:flex">
         <div className="flex items-center justify-between py-3 md:py-6 md:block">
           <Link href="/#home">
             <div className="container flex items-center space-x-2">
-              <h2 className="text-1xl font-bold text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 cursor-pointer mx-2 mr-2">
+              <h2 className="text-1xl font-bold text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 cursor-pointer mr-32">
                 Diego Marulanda
               </h2>
             </div>
@@ -51,7 +51,7 @@ const Navigation = () => {
               navigation ? 'block' : 'hidden'
             }`}
           >
-            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 mr-28">
               {LINKS_NAV.map((item, index) => (
                 <Link key={index} href={item.route} rel="dns-prefetch">
                   <div
