2e49dcd362137a77eea67ea10c29d9b90719ea9a
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-02-16 08:18:30 -0500

    refactor: :sparkles: add new button

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -65,18 +65,18 @@ const Navigation = () => {
               {currentTheme === 'dark' ? (
                 <button
                   onClick={() => setTheme('light')}
-                  className="bg-gradient-to-r from-purple-500 via-purple-500 to-purple-500  rounded-full cursor-pointer px-2 py-2 font-bold"
+                  className="bg-gradient-to-r from-amber-500 via-amber-400 to-amber-500 rounded-full cursor-pointer px-2 py-2 font-bold"
                   aria-label="dark mode button"
                 >
-                  <PiMoonStarsBold size={25} color="white" />
+                  <PiSunHorizonBold size={25} color="white" />
                 </button>
               ) : (
                 <button
                   onClick={() => setTheme('dark')}
-                  className="bg-gradient-to-r from-amber-500 via-amber-400 to-amber-500  rounded-full cursor-pointer px-2 py-2 font-bold"
+                  className="bg-gradient-to-r from-purple-500 via-purple-500 to-purple-500 rounded-full cursor-pointer px-2 py-2 font-bold"
                   aria-label="light mode button"
                 >
-                  <PiSunHorizonBold size={25} color="black" />
+                  <PiMoonStarsBold size={25} color="black" />
                 </button>
               )}
             </div>
