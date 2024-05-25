72b3039e4d7e1dae208cf9f33d56789fab84e525
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-25 17:39:11 -0500

    fix: :bug: add fixes

--- src/components/Skills/MySkills.tsx
+++ src/components/Skills/MySkills.tsx
@@ -4,59 +4,64 @@ import React from 'react'
 const MySkills = () => {
   return (
     <div className="md:w-1/2 mt-[-52px]">
-      <div className=" flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center mr-4 font-extrabold capitalize text-4xl sm:text-6xl md:text-4xl lg:text-4xl xl:text-7xl">
+      <div className=" flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center mr-4 mt-1 font-extrabold capitalize text-4xl sm:text-6xl md:text-4xl lg:text-4xl xl:text-7xl">
         <h2>My Skills</h2>
       </div>
-      <div className="flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center text-2xl mr-2 mt-4 py-2  text-blue-800 dark:text-amber-200">
-        <h2>Technologies</h2>
-      </div>
       <div className="flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center">
-        <div className="grid grid-cols-3 md:grid-cols-2 gap-4 mt-4">
-          <div className="flex flex-col items-center justify-center">
-            <Image
-              src="/w3_css-icon.svg"
-              loading="lazy"
-              alt="Css Icon"
-              width={75}
-              height={75}
-              className="h-auto max-w-full px-1 py-2 mb-1 bg-white dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100"
-            />
-            <span className="justify-center text-center text-1xl">Css</span>
+        {/* copia */}
+        <div className="md:w-3/3">
+          <div className="flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center text-2xl mr-2 mt-2 py-2 text-blue-800 dark:text-amber-200">
+            <h2>Technologies </h2>
           </div>
-          <div className="flex flex-col items-center justify-center">
-            <Image
-              src="/html5-icon.svg"
-              loading="lazy"
-              alt="Html Icon"
-              width={75}
-              height={75}
-              className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg   text-lg xl:text-4xl border-solid border-2 border-slate-100"
-            />
-            <span className="justify-center text-1xl">Html</span>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <Image
-              src="/javascript.svg"
-              loading="lazy"
-              alt="JavaScript Icon"
-              width={75}
-              height={75}
-              className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100"
-            />
-            <span className="justify-center text-1xl">JavaScript</span>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <Image
-              src="/reactjs-icon.svg"
-              loading="lazy"
-              alt="React Icon"
-              width={73}
-              height={73}
-              className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg   text-lg xl:text-4xl border-solid border-2 border-slate-100"
-            />
-            <span className="justify-center text-1xl">React</span>
+          <div className="grid grid-cols-3 md:grid-cols-2 gap-4 mt-4">
+            <div className="flex flex-col items-center justify-center">
+              <Image
+                src="/w3_css-icon.svg"
+                loading="lazy"
+                alt="Git Bash Icon"
+                width={75}
+                height={75}
+                className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100"
+              />
+              <span className="justify-center text-1xl">Css</span>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <Image
+                src="/html5-icon.svg"
+                loading="lazy"
+                alt="Git Icon"
+                width={75}
+                height={75}
+                className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100"
+              />
+              <span className="justify-center text-1xl">Html</span>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <Image
+                src="/javascript.svg"
+                loading="lazy"
+                alt="Github Icon"
+                width={75}
+                height={75}
+                className="h-auto max-w-full px-1 py-2 mb-1 bg-white dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100"
+              />
+              <span className="justify-center text-1xl">Js</span>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <Image
+                src="/reactjs-icon.svg"
+                loading="lazy"
+                alt="Linux Icon"
+                width={75}
+                height={75}
+                className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100"
+              />
+              <span className="justify-center text-center text-1xl">React</span>
+            </div>
           </div>
         </div>
+
+        {/* fin copia */}
         <div className="md:w-3/3">
           <div className="flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center text-2xl mb-1 py-2 text-blue-800 dark:text-amber-200 mt-2">
             <h2>Tools</h2>
