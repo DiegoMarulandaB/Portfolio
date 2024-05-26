96322ee730bee62adcf258e4c6ca3dc93fa2aceb
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-26 10:40:18 -0500

    feat: :sparkles: add new component contact button

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,4 +1,6 @@
+import ContactButton from './ContactButton'
 import React from 'react'
+
 const Contact = () => {
   return (
     <div id="contact">
@@ -31,41 +33,7 @@ const Contact = () => {
                 <div className="pt-16 mr-4 mt-2 mb-4 text-4xl sm:text-6xl md:text-4xl lg:text-4xl xl:text-7xl font-extrabold capitalize flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center">
                   <h2>Contact Me</h2>
                 </div>
-                <div className="flex flex-col-row items-start text-start justify-start md:items-center md:text-center md:justify-center">
-                  <a
-                    href="mailto:%20diegomjobs@gmail.com%20?Subject=Aquí%20el%20asunto%20del%20mail"
-                    className=" text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 inline-flex hover:rounded-lg"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Contact me by sending me an email"
-                  >
-                    <span className="relative text-center cursor-pointer mr-4 mt-2 text-1xl text-white bg-slate-600 hover:bg-slate-950 font-medium rounded-lg text-sm px-2.5 py-2.5 me-1 mb-1 dark:text-slate-950 dark:bg-slate-50 dark:hover:bg-blue-100">
-                      Email →
-                    </span>
-                  </a>
-                  <a
-                    href="https://www.linkedin.com/in/diegomarulandabarrientos/"
-                    className="text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 inline-flex hover:rounded-lg"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Read more about my profile on Linkedin"
-                  >
-                    <span className="relative text-center cursor-pointer mr-4 mt-2 text-1xl text-white bg-slate-600 hover:bg-slate-950 font-medium rounded-lg text-sm px-2.5 py-2.5 me-1 mb-1 dark:text-slate-950 dark:bg-slate-50 dark:hover:bg-blue-100">
-                      Linkedin →
-                    </span>
-                  </a>
-                  <a
-                    href="https://github.com/DiegoMarulandaB"
-                    className="text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 inline-flex hover:rounded-lg"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Read more about my Github profile"
-                  >
-                    <span className="relative text-center cursor-pointer mr-4 mt-2 text-1xl text-white bg-slate-600 hover:bg-slate-950 font-medium rounded-lg text-sm px-2.5 py-2.5 me-1 mb-1 dark:text-slate-950 dark:bg-slate-50 dark:hover:bg-blue-100">
-                      Github →
-                    </span>
-                  </a>
-                </div>
+                <ContactButton />
               </div>
             </div>
           </div>
---
+++ src/components/Contact/ContactButton.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const ContactButton = () => {
+  return (
+    <div className="flex flex-col-row items-start text-start justify-start md:items-center md:text-center md:justify-center">
+      <a
+        href="mailto:%20diegomjobs@gmail.com%20?Subject=Aquí%20el%20asunto%20del%20mail"
+        className=" text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 inline-flex hover:rounded-lg"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Contact me by sending me an email"
+      >
+        <span className="relative text-center cursor-pointer mr-4 mt-2 text-1xl text-white bg-slate-600 hover:bg-slate-950 font-medium rounded-lg text-sm px-2.5 py-2.5 me-1 mb-1 dark:text-slate-950 dark:bg-slate-50 dark:hover:bg-blue-100">
+          Email →
+        </span>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/diegomarulandabarrientos/"
+        className="text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 inline-flex hover:rounded-lg"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Read more about my profile on Linkedin"
+      >
+        <span className="relative text-center cursor-pointer mr-4 mt-2 text-1xl text-white bg-slate-600 hover:bg-slate-950 font-medium rounded-lg text-sm px-2.5 py-2.5 me-1 mb-1 dark:text-slate-950 dark:bg-slate-50 dark:hover:bg-blue-100">
+          Linkedin →
+        </span>
+      </a>
+      <a
+        href="https://github.com/DiegoMarulandaB"
+        className="text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 inline-flex hover:rounded-lg"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Read more about my Github profile"
+      >
+        <span className="relative text-center cursor-pointer mr-4 mt-2 text-1xl text-white bg-slate-600 hover:bg-slate-950 font-medium rounded-lg text-sm px-2.5 py-2.5 me-1 mb-1 dark:text-slate-950 dark:bg-slate-50 dark:hover:bg-blue-100">
+          Github →
+        </span>
+      </a>
+    </div>
+  )
+}
+
+export default ContactButton
