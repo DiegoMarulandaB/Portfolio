8664586660529abee496583cfc6f87bab89655cb
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-26 11:15:46 -0500

    feat: :sparkles: add new themeButton and mobile button components

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,25 +1,12 @@
-import React, { useState } from 'react'
-import { useTheme } from 'next-themes'
 import { usePathname } from 'next/navigation'
-import { PiMoonStarsBold, PiSunHorizonBold } from 'react-icons/pi'
-import { IoMdMenu, IoMdClose } from 'react-icons/io'
 import Link from 'next/link'
-import NaviLink from '../../types/NaviLink'
-
-const LINKS_NAV: NaviLink[] = [
-  { label: 'Home', route: '/#home' },
-  { label: 'Experience', route: '/#experience' },
-  { label: 'Projects', route: '/#projects' },
-  { label: 'About Me', route: '/#about' },
-  { label: 'Contact Me', route: '/#contact' },
-]
+import ThemeButton from './ThemeButton'
+import MobileButton from './MobileButton'
+import React from 'react'
 
 const Navigation = () => {
-  const { systemTheme, theme, setTheme } = useTheme()
-  const currentTheme = theme === 'system' ? systemTheme : theme === 'dark' ? 'dark' : 'light'
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const pathname = usePathname()
-  const [navigation, setNavigation] = useState(false)
 
   return (
     <header className="w-full mx-auto px-6 sm:px-20 opacity-90 fixed top-0 z-50 shadow bg-slate-100 dark:bg-slate-700 dark:border-slate-500">
@@ -32,56 +19,10 @@ const Navigation = () => {
               </h2>
             </div>
           </Link>
-          {/* mobile menu */}
-          <div className="md:hidden">
-            <button
-              onClick={() => {
-                setNavigation(!navigation)
-              }}
-              className="p-2 bg-gradient-to-r text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 cursor-pointer px-2 py-2 text-1xl  font-bold "
-              aria-label="mobile menu button"
-            >
-              {navigation ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
-            </button>
-          </div>
+          <MobileButton />
         </div>
         <div>
-          <div
-            // menu
-            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 cursor-pointer text-1xl text-slate-700 hover:text-slate-950  ${
-              navigation ? 'block' : 'hidden'
-            }`}
-          >
-            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 mr-28">
-              {LINKS_NAV.map((item, index) => (
-                <Link key={index} href={item.route} rel="dns-prefetch">
-                  <div
-                    className="block lg:inline-block text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 "
-                    onClick={() => setNavigation(!navigation)}
-                  >
-                    {item.label}
-                  </div>
-                </Link>
-              ))}
-              {currentTheme === 'dark' ? (
-                <button
-                  onClick={() => setTheme('light')}
-                  className="bg-gradient-to-r from-amber-500 via-amber-400 to-amber-500 rounded-full cursor-pointer px-2 py-2 font-bold"
-                  aria-label="dark mode button"
-                >
-                  <PiSunHorizonBold size={25} color="white" />
-                </button>
-              ) : (
-                <button
-                  onClick={() => setTheme('dark')}
-                  className="bg-gradient-to-r from-purple-500 via-purple-500 to-purple-500 rounded-full cursor-pointer px-2 py-2 font-bold"
-                  aria-label="light mode button"
-                >
-                  <PiMoonStarsBold size={25} color="black" />
-                </button>
-              )}
-            </div>
-          </div>
+          <ThemeButton />
         </div>
       </div>
     </header>
---
+++ src/components/Navigation/ThemeButton.tsx
@@ -0,0 +1,59 @@
+import { useTheme } from 'next-themes'
+import { PiMoonStarsBold, PiSunHorizonBold } from 'react-icons/pi'
+import NaviLink from '../../types/NaviLink'
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+const LINKS_NAV: NaviLink[] = [
+  { label: 'Home', route: '/#home' },
+  { label: 'Experience', route: '/#experience' },
+  { label: 'Projects', route: '/#projects' },
+  { label: 'About Me', route: '/#about' },
+  { label: 'Contact Me', route: '/#contact' },
+]
+const ThemeButton = () => {
+  const { systemTheme, theme, setTheme } = useTheme()
+  const currentTheme = theme === 'system' ? systemTheme : theme === 'dark' ? 'dark' : 'light'
+  const [navigation, setNavigation] = useState(false)
+
+  return (
+    <div
+      // menu
+      className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 cursor-pointer text-1xl text-slate-700 hover:text-slate-950  ${
+        navigation ? 'block' : 'hidden'
+      }`}
+    >
+      <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 mr-28">
+        {LINKS_NAV.map((item, index) => (
+          <Link key={index} href={item.route} rel="dns-prefetch">
+            <div
+              className="block lg:inline-block text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 "
+              onClick={() => setNavigation(!navigation)}
+            >
+              {item.label}
+            </div>
+          </Link>
+        ))}
+        {currentTheme === 'dark' ? (
+          <button
+            onClick={() => setTheme('light')}
+            className="bg-gradient-to-r from-amber-500 via-amber-400 to-amber-500 rounded-full cursor-pointer px-2 py-2 font-bold"
+            aria-label="dark mode button"
+          >
+            <PiSunHorizonBold size={25} color="white" />
+          </button>
+        ) : (
+          <button
+            onClick={() => setTheme('dark')}
+            className="bg-gradient-to-r from-purple-500 via-purple-500 to-purple-500 rounded-full cursor-pointer px-2 py-2 font-bold"
+            aria-label="light mode button"
+          >
+            <PiMoonStarsBold size={25} color="black" />
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default ThemeButton
