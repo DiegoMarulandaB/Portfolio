07771ea482d4891e5c99203f55902c8d479837ac
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-27 12:15:12 -0500

    feat: :sparkles: add new components of technology_tools_learning

---
+++ src/components/Skills/Learning.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Learning = () => {
+  return (
+    <div className="md:w-3/3">
+      <div className="flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center text-2xl mt-7 mb-1 py-2  text-blue-800 dark:text-amber-200">
+        <h2>Learning</h2>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-2 gap-4 mt-6">
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="assets/svg/tailwindcss.svg"
+            loading="lazy"
+            alt="Tailwind Icon"
+            width={100}
+            height={100}
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+          />
+          <span className="justify-center text-1xl"></span>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="assets/svg/typescriptlang.svg"
+            loading="lazy"
+            alt="Typescript Icon"
+            width={100}
+            height={100}
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+          />
+          <span className="justify-center text-1xl"></span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Learning
---
+++ src/components/Skills/Technologies.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Technologies = () => {
+  return (
+    <div className="md:w-3/3">
+      <div className="flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center text-2xl mr-2 mt-2 py-2 text-blue-800 dark:text-amber-200">
+        <h2>Technologies </h2>
+      </div>
+      <div className="grid grid-cols-3 md:grid-cols-2 gap-4 mt-8">
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="assets/svg/css.svg"
+            loading="lazy"
+            alt="Git Bash Icon"
+            width={100}
+            height={100}
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+          />
+          <span className="justify-center text-1xl"></span>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="assets/svg/html5.svg"
+            loading="lazy"
+            alt="Git Icon"
+            width={100}
+            height={100}
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+          />
+          <span className="justify-center text-1xl"></span>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="assets/svg/javascript.svg"
+            loading="lazy"
+            alt="JavaScript Icon"
+            width={100}
+            height={100}
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+          />
+          <span className="justify-center text-1xl"></span>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="assets/svg/reactjs.svg"
+            loading="lazy"
+            alt="React Icon"
+            width={100}
+            height={100}
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+          />
+          <span className="justify-center text-center text-1xl"></span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Technologies
