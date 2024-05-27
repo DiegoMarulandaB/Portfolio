4868d2c85fd9bcfdf344a1bb2e1251dff1c6e9c6
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-27 08:57:52 -0500

    refactor: :bug: add fixes code

--- src/components/Contact/ContactButton.tsx
+++ src/components/Contact/ContactButton.tsx
@@ -5,10 +5,10 @@ const ContactButton = () => {
     <div className="flex flex-col-row items-start text-start justify-start md:items-center md:text-center md:justify-center">
       <a
         href="mailto:%20diegomjobs@gmail.com%20?Subject=Aquí%20el%20asunto%20del%20mail"
-        className=" text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 inline-flex hover:rounded-lg"
+        className="text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 inline-flex hover:rounded-lg"
         target="_blank"
         rel="noopener noreferrer"
-        aria-label="Contact me by sending me an email"
+        aria-label="Contact me by email"
       >
         <span className="relative text-center cursor-pointer mr-4 mt-2 text-1xl text-white bg-slate-600 hover:bg-slate-950 font-medium rounded-lg text-sm px-2.5 py-2.5 me-1 mb-1 dark:text-slate-950 dark:bg-slate-50 dark:hover:bg-blue-100">
           Email →
@@ -19,7 +19,7 @@ const ContactButton = () => {
         className="text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 inline-flex hover:rounded-lg"
         target="_blank"
         rel="noopener noreferrer"
-        aria-label="Read more about my profile on Linkedin"
+        aria-label="Visit my LinkedIn profile"
       >
         <span className="relative text-center cursor-pointer mr-4 mt-2 text-1xl text-white bg-slate-600 hover:bg-slate-950 font-medium rounded-lg text-sm px-2.5 py-2.5 me-1 mb-1 dark:text-slate-950 dark:bg-slate-50 dark:hover:bg-blue-100">
           Linkedin →
@@ -30,7 +30,7 @@ const ContactButton = () => {
         className="text-slate-700 hover:text-slate-950 dark:text-white dark:hover:text-blue-100 inline-flex hover:rounded-lg"
         target="_blank"
         rel="noopener noreferrer"
-        aria-label="Read more about my Github profile"
+        aria-label="Visit my GitHub profile"
       >
         <span className="relative text-center cursor-pointer mr-4 mt-2 text-1xl text-white bg-slate-600 hover:bg-slate-950 font-medium rounded-lg text-sm px-2.5 py-2.5 me-1 mb-1 dark:text-slate-950 dark:bg-slate-50 dark:hover:bg-blue-100">
           Github →
