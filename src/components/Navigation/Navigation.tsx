b7a0b2b07298a149ec6e43c78d8db7dec2bcb4de
Diego Marulanda B <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-29 10:36:22 -0500

    refactor: :sparkles: add new dark:bg-slate-700

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -10,7 +10,7 @@ const Navigation = () => {
   const [navigation, setNavigation] = useState(false)
 
   return (
-    <header className="w-full mx-auto px-6 sm:px-20 opacity-90 fixed top-0 z-50 shadow bg-slate-100 dark:bg-slate-700 dark:border-slate-500">
+    <header className="w-full mx-auto px-6 sm:px-20 opacity-90 fixed top-0 z-50 shadow bg-slate-100 dark:bg-slate-800 dark:border-slate-500">
       <div className="justify-center md:items-center md:flex">
         <div className="flex items-center justify-between py-3 md:py-6 md:block">
           <Link href="/#home">
