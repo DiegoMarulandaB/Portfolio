edb3e69326d30fba1355940cf38bdb084dbc7a77
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-30 10:53:44 -0500

    refactor: :sparkles: add new rounded  svg

--- src/components/Skills/Learning.tsx
+++ src/components/Skills/Learning.tsx
@@ -15,7 +15,7 @@ const Learning = () => {
             alt="Tailwind Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-1xl"></span>
         </div>
@@ -26,7 +26,7 @@ const Learning = () => {
             alt="Typescript Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-1xl"></span>
         </div>
--- src/components/Skills/Tools.tsx
+++ src/components/Skills/Tools.tsx
@@ -15,7 +15,7 @@ const Tools = () => {
             alt="Git Bash Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-1xl"></span>
         </div>
@@ -26,7 +26,7 @@ const Tools = () => {
             alt="Docker Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-1xl"></span>
         </div>
@@ -37,7 +37,7 @@ const Tools = () => {
             alt="Git Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-1xl"></span>
         </div>
@@ -48,7 +48,7 @@ const Tools = () => {
             alt="Github Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-1xl"></span>
         </div>
@@ -59,7 +59,7 @@ const Tools = () => {
             alt="GitLab Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-1xl"></span>
         </div>
@@ -70,7 +70,7 @@ const Tools = () => {
             alt="Kubernetes Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-center text-1xl"></span>
         </div>
@@ -81,7 +81,7 @@ const Tools = () => {
             alt="Linux Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-center text-1xl"></span>
         </div>
@@ -92,7 +92,7 @@ const Tools = () => {
             alt="Mysql Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-center text-1xl"></span>
         </div>
@@ -103,7 +103,7 @@ const Tools = () => {
             alt="Neovim Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-1xl"></span>
         </div>
@@ -114,7 +114,7 @@ const Tools = () => {
             alt="Postman Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-1xl"></span>
         </div>
@@ -125,7 +125,7 @@ const Tools = () => {
             alt="Vscode Icon"
             width={100}
             height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded-lg  text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-1xl"></span>
         </div>
