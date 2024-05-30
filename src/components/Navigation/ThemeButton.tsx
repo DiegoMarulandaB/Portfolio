266c488220f8b76c95359fc200571cc71cb2c27b
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-30 10:45:31 -0500

    refactor: :sparkles: add new rounded-g button

--- src/components/Navigation/ThemeButton.tsx
+++ src/components/Navigation/ThemeButton.tsx
@@ -19,7 +19,7 @@ const ThemeButton = () => {
       {currentTheme === 'dark' ? (
         <button
           onClick={() => setTheme('light')}
-          className="bg-gradient-to-r from-purple-500 via-purple-500 to-purple-500 rounded-full cursor-pointer px-2 py-2 font-bold"
+          className="bg-gradient-to-r from-purple-500 via-purple-500 to-purple-500 rounded-lg cursor-pointer px-2 py-2 font-bold"
           aria-label="dark mode button"
         >
           <PiMoonStarsBold size={25} color="black" />
@@ -27,7 +27,7 @@ const ThemeButton = () => {
       ) : (
         <button
           onClick={() => setTheme('dark')}
-          className="bg-amber-300 rounded-full cursor-pointer px-2 py-2 font-bold"
+          className="bg-amber-300 rounded-lg cursor-pointer px-2 py-2 font-bold"
           aria-label="light mode button"
         >
           <PiSunHorizonBold size={25} color="white" />
