9dafb19c0ed557ba1fc05d36b35fbd75bbc3023f
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-31 14:50:25 -0500

    refactor: :sparkles: add new footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,13 +4,11 @@ import AllRightsReserved from './AllRightsReserved'
 const Footer = () => {
 
   return (
-    <footer id="footer" className="mt-7">
-      <div className="mx-auto max-w-full md:max-w-full sm:px-1">
-        <div className="border-t border-slate-800 dark:border-slate-50">
-          <div className="mx-auto flex flex-col md:flex-row justify-start md:items-center md:text-center md:justify-center space-x-2 ml-6 mr-6">
-            <Year />
-            <AllRightsReserved />
-          </div>
+    <footer id="footer" className="mx-auto max-w-full md:max-w-full sm:px-1">
+      <div className="border-t border-slate-800 dark:border-slate-50">
+        <div className="mx-auto flex flex-col md:flex-row justify-start md:items-center md:text-center md:justify-center space-x-2 ml-6 mr-6">
+          <Year />
+          <AllRightsReserved />
         </div>
       </div>
     </footer>
@@ -18,3 +16,22 @@ const Footer = () => {
 }
 
 export default Footer
+
+// import React from 'react'
+// import Year from './Year'
+// import AllRightsReserved from './AllRightsReserved'
+
+// const Footer = () => {
+//   return (
+//     <footer id="footer" className="mx-auto max-w-full sm:px-4">
+//       <div className="border-t border-slate-800 dark:border-slate-50 sm:border-t-0">
+//         <div className="mx-auto flex flex-col md:flex-row justify-start md:items-center md:text-center md:justify-center space-x-2 ml-6 mr-6">
+//           <Year />
+//           <AllRightsReserved />
+//         </div>
+//       </div>
+//     </footer>
+//   )
+// }
+
+// export default Footer
