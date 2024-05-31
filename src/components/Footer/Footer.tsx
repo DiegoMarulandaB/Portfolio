417fe842dfab761235afe77539818e9e2754f20d
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-31 16:12:21 -0500

    refactor: :fire: add delete code

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,22 +16,3 @@ const Footer = () => {
 }
 
 export default Footer
-
-// import React from 'react'
-// import Year from './Year'
-// import AllRightsReserved from './AllRightsReserved'
-
-// const Footer = () => {
-//   return (
-//     <footer id="footer" className="mx-auto max-w-full sm:px-4">
-//       <div className="border-t border-slate-800 dark:border-slate-50 sm:border-t-0">
-//         <div className="mx-auto flex flex-col md:flex-row justify-start md:items-center md:text-center md:justify-center space-x-2 ml-6 mr-6">
-//           <Year />
-//           <AllRightsReserved />
-//         </div>
-//       </div>
-//     </footer>
-//   )
-// }
-
-// export default Footer
