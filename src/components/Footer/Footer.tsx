4a18c79f88eb70f540d1bbd06eee09f3a379b0d9
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-31 14:39:03 -0500

    refactor: :bug: add fixes mt

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,16 @@
+import React from 'react'
 import Year from './Year'
 import AllRightsReserved from './AllRightsReserved'
-import React from 'react'
+const Footer = () => {
 
-const Footer: React.FC = () => {
   return (
-    <footer id="footer" className="mx-auto max-w-full md:max-w-full sm:px-1">
-      <div className="border-t border-slate-800 dark:border-slate-50">
-        <div className="mx-auto flex flex-col md:flex-row justify-start md:items-center md:text-center md:justify-center space-x-2">
-          <Year />
-          <AllRightsReserved />
+    <footer id="footer" className="mt-7">
+      <div className="mx-auto max-w-full md:max-w-full sm:px-1">
+        <div className="border-t border-slate-800 dark:border-slate-50">
+          <div className="mx-auto flex flex-col md:flex-row justify-start md:items-center md:text-center md:justify-center space-x-2 ml-6 mr-6">
+            <Year />
+            <AllRightsReserved />
+          </div>
         </div>
       </div>
     </footer>
