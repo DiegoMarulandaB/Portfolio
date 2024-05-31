7fe4aad0b95beacbaf62f0b748e23bca5782da4a
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-31 15:49:24 -0500

    refactor: :sparkles: add new mt-8

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
     <footer id="footer" className="mx-auto max-w-full md:max-w-full sm:px-1">
-      <div className="border-t border-slate-800 dark:border-slate-50">
+      <div className="border-t border-slate-800 dark:border-slate-50 mt-8">
         <div className="mx-auto flex flex-col md:flex-row justify-start md:items-center md:text-center md:justify-center space-x-2 ml-6 mr-6">
           <Year />
           <AllRightsReserved />
