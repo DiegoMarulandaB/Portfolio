aa83189384e29089e9def3519dd4f6a457216592
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-05-31 13:33:31 -0500

    refactor: :bug: add fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <footer id="footer" className="mx-auto max-w-full md:max-w-full px-4 sm:px-1 lg:px-1 py-4">
+    <footer id="footer" className="mx-auto max-w-full md:max-w-full sm:px-1">
       <div className="border-t border-slate-800 dark:border-slate-50">
         <div className="mx-auto flex flex-col md:flex-row justify-start md:items-center md:text-center md:justify-center space-x-2">
           <Year />
--- src/components/Footer/Year.tsx
+++ src/components/Footer/Year.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Year = () => {
-    const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear()
   return (
     <div className="flex flex-row space-x-2 mb-2 mt-2">
       <span className="text-slate-800 dark:text-white">&copy;&nbsp;2023-{currentYear}</span>
