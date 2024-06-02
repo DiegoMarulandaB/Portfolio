d95a4ecaa5e529a7ad58dad8d9d4cd49246df9de
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-02 15:47:54 -0500

    fix: :sparkles: add fixes website url

--- src/app/metadata.tsx
+++ src/app/metadata.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
     'Author Diego Marulanda. Portfolio made with TypeScript, Tailwind, Next.js 14. Where you will find different projects, whether academic or personal, related to programming.',
   keywords:
     'Next.js 14,React,TypeScript,Tailwind, Frontend Developer, Html, Css, JavaScript, Git, Github, Linux, Neovim, Visual Studio Code, Bash',
-  metadataBase: new URL('http://localhost:3000'),
+  metadataBase: new URL('https://diegomarulanda.vercel.app/'),
   alternates: {
     canonical: '/',
     languages: {
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     type: 'website',
     images: [
       {
-        url: new URL('./opengraph-image.png', 'http://localhost:3000'),
+        url: new URL('./opengraph-image.png', 'https://diegomarulanda.vercel.app/'),
         width: 1200,
         height: 630,
         alt: 'Open Graph Image Alt',
@@ -34,6 +34,6 @@ export const metadata: Metadata = {
     title: 'Diego Marulanda',
     description:
       'Portfolio where you will find different projects, whether academic or personal, related to programming.',
-    images: [new URL('./twitter-image.png', 'http://localhost:3000')],
+    images: [new URL('./twitter-image.png', 'https://diegomarulanda.vercel.app/')],
   },
 }
