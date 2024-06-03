e6c57015b7331f2ebc0dbeceba76a47078bec1da
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-03 16:41:12 -0500

    feat: :sparkles: add new favicon all divice and site.webmanifiest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="./manifest.json" />
+        <link rel="android-chrome" type="image/png" sizes="192x192" href="./android-chrome-192x192.png" />
+        <link rel="android-chrome" type="image/png" sizes="512x512" href="./android-chrome-512x512.jpg" />
+        <link rel="manifest" href="./site.webmanifest" />
         <link rel="canonical" href="https://diegomarulanda.vercel.app/" />
         <title>DiegoMarulanda</title>
         <meta
