3d8128a32c50c78030e996d573668c72bc88cd63
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-03 15:33:41 -0500

    feat: :sparkles: add url and new images from opengraph _twitter for the metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
     'Portfolio where you will find different projects, whether academic or personal, related to programming.',
   keywords:
     'Next.js 13,React,TypeScript,Tailwind, Frontend Developer, Html, Css, JavaScript, Git, Github, Linux, Neovim,Visual Studio Code, Bash',
-  metadataBase: new URL('https://diegomarulanda.vercel.app'),
+  metadataBase: new URL('https://diegomarulanda.vercel.app/'),
   alternates: {
     canonical: '/',
     languages: {
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     },
   },
   openGraph: {
-    images: '/og-image.jpg',
+    images: 'https://diegomarulanda.vercel.app/opengraph-image.jpg',
   },
 }
 
@@ -29,6 +29,7 @@ const popps = Poppins({
   subsets: ['latin'],
   display: 'swap',
 })
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -37,16 +38,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        {/* canonical */}
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="canonical" href="https://diegomarulanda.vercel.app" />
+        <link rel="canonical" href="https://diegomarulanda.vercel.app/" />
         <title>DiegoMarulanda</title>
         <meta
           name="description"
           content="Portfolio where you will find different projects with different programming languages, such as Css, Html, JavaScript, React, TypeScript.
           You can also see academic projects, personal projects. with their respective repository on Github"
         ></meta>
-        {/* op */}
         <meta property="og:title" content="Portfolio Diego Marulanda" />
         <meta property="og:image" content="https://diegomarulanda.vercel.app/opengraph-image.jpg" />
         <meta property="og:image:alt" content="Programming portfolio" />
@@ -54,11 +53,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           property="og:description"
           content="Portfolio where you will find different projects with different programming languages, such as Css, Html, JavaScript, React, TypeScript"
         />
-        <meta property="og:site_name" content="https://diegomarulanda.vercel.app" />
+        <meta property="og:site_name" content="diegomarulanda.vercel.app" />
         <meta property="og:image:type" content="image/jpg" />
-        <meta property="og:image:width" content="565" />
-        <meta property="og:image:height" content="296" />
-        {/* twitter */}
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="twitter:title" content="Portfolio Diego Marulanda" />
         <meta name="twitter:image" content="https://diegomarulanda.vercel.app/twitter-image.jpg" />
         <meta property="twitter:image:alt" content="Programming portfolio" />
@@ -66,10 +64,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           property="twitter:description"
           content="Portfolio where you will find different projects with different programming languages, such as Css, Html, JavaScript, React, TypeScript"
         />
-        <meta property="twitter:site_name" content="https://diegomarulanda.vercel.app" />
+        <meta property="twitter:site_name" content="diegomarulanda.vercel.app" />
         <meta name="twitter:image:type" content="image/jpg" />
-        <meta name="twitter:image:width" content="565" />
-        <meta name="twitter:image:height" content="296" />
+        <meta name="twitter:image:width" content="1200" />
+        <meta name="twitter:image:height" content="630" />
       </head>
       <body className={`${popps.className} ligth-bg-theme dark-bg-theme`}>
         <ThemeProvider enableSystem={true} attribute="class">
