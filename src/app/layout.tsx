7021d2a80500c1afd88b64cf3ae802e21cbd7b9f
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-03 18:05:00 -0500

    feat: :sparkles: add new images in the public folder and new url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
       'Portfolio where you will find different projects with different programming languages, such as Css, Html, JavaScript, React, TypeScript.',
     images: [
       {
-        url: './opengraph-image.jpg',
+        url: 'https://diegomarulanda.vercel.app/opengraph-image.jpg',
         width: 1200,
         height: 630,
         alt: 'Programming portfolio',
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
       'Portfolio where you will find different projects with different programming languages, such as Css, Html, JavaScript, React, TypeScript.',
     images: [
       {
-        url: './twitter-image.jpg',
+        url: 'https://diegomarulanda.vercel.app/twitter-image.jpg',
         width: 1200,
         height: 630,
         alt: 'Programming portfolio',
