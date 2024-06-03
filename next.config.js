6413b9c5c76014f00be87364c8cb5b4faaa842d4
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-03 15:35:32 -0500

    refactor: :bug: add fixes url

--- next.config.js
+++ next.config.js
@@ -7,10 +7,9 @@ const nextConfig = {
     // loader: "custom",
     // loaderFile: "./my/image/loader.js",
     unoptimized: true,
-    domains: ['https://diegomarulanda.vercel.app'],
+    domains: ['https://diegomarulanda.vercel.app/'],
   },
-  pageExtensions: ['page.tsx','page.ts','route/tsx','route.ts','tsx'],
+  pageExtensions: ['page.tsx', 'page.ts', 'route/tsx', 'route.ts', 'tsx'],
 }
 
 module.exports = nextConfig
-
