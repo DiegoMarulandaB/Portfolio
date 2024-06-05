eebac7acde6605548b44b6b5cf8c2b3f2d1fb796
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-04 19:23:31 -0500

    refactor: :sparkles: add fixes to the domain url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
       'Portfolio where you will find different projects with different programming languages, such as Css, Html, JavaScript, React, TypeScript.',
     images: [
       {
-        url: './opengraph-imagepng.png',
+        url: 'https://diegomarulanda.vercel.app/opengraph-imagepng.png',
         width: 1200,
         height: 630,
         alt: 'opengraph image from my portfolio',
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
       'Portfolio where you will find different projects with different programming languages, such as Css, Html, JavaScript, React, TypeScript.',
     images: [
       {
-        url: './twitter-image.png',
+        url: 'https://diegomarulanda.vercel.app/twitter-image.png',
         width: 1200,
         height: 630,
         alt: 'image to post on twitter from my portfolio',
