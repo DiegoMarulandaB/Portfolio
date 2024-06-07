46ce9452a5cbed39f6ad6623641546bea97cf6be
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-07 14:38:30 -0500

    refactor: :sparkles: add new color vertical line

--- src/components/About/TimeLine.tsx
+++ src/components/About/TimeLine.tsx
@@ -22,10 +22,10 @@ const TimeLine = () => {
         {/* Card 2 */}
         <div className="transform transition ml-10 relative flex items-center px-2 py-1  text-slate-800 dark:text-white mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
           {/* Dot Follwing the Left Vertical Line*/}
-          <div className="w-5 h-5 bg-blue-800 dark:bg-amber-200  absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-1 md:mt-0"></div>
+          <div className="w-5 h-5 bg-slate-800 dark:bg-slate-50   absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-1 md:mt-0"></div>
 
           {/* Line that connecting the box with the vertical line */}
-          <div className="w-10 h-1 bg-blue-800 dark:bg-amber-200 absolute -left-10 z-0"></div>
+          <div className="w-10 h-1 bg-slate-800 dark:bg-slate-50  absolute -left-10 z-0"></div>
           {/*Content that showing in the box*/}
           <div className="flex-auto">
             <h3 className="heading-3-studies-timeline">Digital House 2021 - 2023</h3>
@@ -37,9 +37,9 @@ const TimeLine = () => {
         {/* Card 3 */}
         <div className="transform transition ml-10 relative flex items-center px-2 py-1  text-slate-800 dark:text-white mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
           {/* Dot Follwing the Left Vertical Line*/}
-          <div className="w-5 h-5 bg-blue-800 dark:bg-amber-200  absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-1 md:mt-0"></div>
+          <div className="w-5 h-5 bg-slate-800 dark:bg-slate-50   absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-1 md:mt-0"></div>
           {/* Line that connecting the box with the vertical line */}
-          <div className="w-10 h-1 bg-blue-800 dark:bg-amber-200  absolute -left-10 z-0"></div>
+          <div className="w-10 h-1 bg-slate-800 dark:bg-slate-50   absolute -left-10 z-0"></div>
           {/* Content that showing in the box*/}
           <div className="flex-auto">
             <h3 className="heading-3-studies-timeline">Alura Latam 2022 - 2022</h3>
