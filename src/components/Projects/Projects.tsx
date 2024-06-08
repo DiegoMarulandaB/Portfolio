d3fad1bc5a61f6ec4c9a03973b3fbd78ec73d387
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-08 14:08:38 -0500

    refactor: :bug: add fixes to project cards

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -6,7 +6,8 @@ import MenuButtonAllProjects from './MenuButtonAllProjects'
 const Projects: React.FC = () => {
   return (
     <section id="projects" className="pt-28 mt-[-94px]">
-      <div className="max-w-screen-lg mx-auto px-4 sm:px-6 lg:px-8">
+      {/* <div className="max-w-screen-lg mx-auto px-4 sm:px-6 lg:px-8"> */}
+      <div className="mx-auto lg:px-8 max-w-screen-lg sm:px-6">
         <div className="text-start justify-start">
           <h2 className="heading-2 heading-2-underline ml-2">Projects</h2>
         </div>
