ed52a5e6a1fa107a5f4d6b6b8b6dd9f4be78a9a4
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-09 16:07:21 -0500

    refactor: :bug: add New_sizes_projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,7 +5,7 @@ import MenuButtonAllProjects from './MenuButtonAllProjects'
 
 const Projects: React.FC = () => {
   return (
-    <section id="projects" className="pt-28 mt-[-92px]">
+    <section id="projects" className="pt-28 mt-[-88px]">
       {/* <div className="max-w-screen-lg mx-auto px-4 sm:px-6 lg:px-8"> */}
       <div className="mx-auto lg:px-8 max-w-screen-lg sm:px-6">
         <div className="text-start justify-start">
