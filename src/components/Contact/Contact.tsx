b821885ca0fbc41c8b04e44e446aa5cd0d104592
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-09 11:53:28 -0500

    feat: :sparkles: add new responsive contact

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,28 +3,28 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div id="contact" className="pt-28 mt-[-72px]">
+    <div id="contact" className="pt-28 mt-[-88px]">
       <div className="mx-auto lg:px-8 max-w-screen-lg sm:px-6">
         <div className="flex items-start md:items-start md:justify-start justify-between text-start">
           <div className="flex flex-col ml-2 mx-1 mr-1">
             <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-1">
-              <div className="md:w-6/12">
-                <div className="paragraph paragraph-dark-theme [&>p]:mb-4">
-                  <div className="flex mt-4">
-                    <div className="flex-shrink-0 w-16 h-16 md:w-18 md:h-18 mb-4">
+              <div className="w-full md:w-6/12">
+                <div className="paragraph [&>p]:mb-4">
+                  <div className="flex flex-col sm:flex-row mt-4">
+                    <div className="flex-shrink-0 w-16 h-16 md:w-18 md:h-18 mb-4 sm:mb-0">
                       <img
                         loading="lazy"
-                        className="img-contact"
+                        className="img-contact w-full h-full object-cover"
                         width={776}
                         height={724}
                         src="/assets/webp/Diego.webp"
                         alt="Profile Picture"
                       />
                     </div>
-                    <div className="ml-2">
+                    <div className="mt-2 sm:mt-0 sm:ml-4">
                       <h1 className="heading-1">
-                        Hi!{''} 👋🏽 <strong className="description-user heading-1">I&apos;m Diego</strong>, a passionate
-                        freelancer frontend developer from Colombia! {''}🇨🇴{' '}
+                        Hi! 👋🏽 <strong className="description-user heading-1">I&apos;m Diego</strong>, a passionate
+                        freelancer frontend developer from Colombia! 🇨🇴
                       </h1>
                     </div>
                   </div>
