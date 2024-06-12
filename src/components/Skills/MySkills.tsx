21781302c45ee8d82dd00a47ac0297fae76e0783
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-12 14:32:39 -0500

    feat: :sparkles: underline-myskills-mobile

--- src/components/Skills/MySkills.tsx
+++ src/components/Skills/MySkills.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const MySkills = () => {
   return (
     <div className="w-full pt-28 mt-[-176px]">
-      <div className="span-contact-experience-about  flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center mr-4 mt-1">
+      <div className="span-contact-experience-about underline-myskills-mobile  flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center mr-4 mt-1">
         <span>My skills</span>
       </div>
       <div className="flex flex-col items-start text-start justify-start md:items-center md:text-center md:justify-center">
