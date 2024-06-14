08930ccaa6f43127ff69291bdb74be7986af564e
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-13 21:20:58 -0500

    refactor: :sparkles: add new margin-left

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ import FrontEnd from './FrontEnd'
 
 const Footer = () => {
   return (
-    <footer id="footer" className="py-4 mt-5 md:mt-[-36px]">
+    <footer id="footer" className="py-4 mt-5 md:mt-[-36px] ml-4 sm:ml-0">
       <div className="mx-auto max-w-full md:max-w-4xl sm:px-1 px-6">
         <div className="border-t border-slate-200 dark:border-slate-700 mx-auto max-w-full md:max-w-full">
           <div className="flex flex-col md:flex-row justify-start items-start md:items-center space-y-2 md:space-y-0 md:space-x-4">
