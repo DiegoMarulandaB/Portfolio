fdd8bf10d41ffd8ab4cf95336aa13c7966aff2f4
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-10-17 14:21:28 -0500

    refactor: :bug: add fix icon

--- src/components/Skills/Tools.tsx
+++ src/components/Skills/Tools.tsx
@@ -60,8 +60,8 @@ const Tools = () => {
         </div>
         <div className="flex flex-col items-center justify-center mt-2">
           <Image
-            src="assets/svg/kubernetes.svg"
-            alt="Kubernetes Icon"
+            src="assets/svg/jestj.svg"
+            alt="Jest Icon"
             width={100}
             height={100}
             className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
@@ -70,8 +70,8 @@ const Tools = () => {
         </div>
         <div className="flex flex-col items-center justify-center mt-2">
           <Image
-            src="assets/svg/jestj.svg"
-            alt="Jest Icon"
+            src="assets/svg/kubernetes.svg"
+            alt="Kubernetes Icon"
             width={100}
             height={100}
             className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
