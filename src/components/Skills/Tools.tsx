c0864d70de92d9ab75836cd01efbfb2469979e18
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-10-17 14:15:07 -0500

    feat: :sparkles: add new icons in tools

--- src/components/Skills/Tools.tsx
+++ src/components/Skills/Tools.tsx
@@ -70,8 +70,28 @@ const Tools = () => {
         </div>
         <div className="flex flex-col items-center justify-center mt-2">
           <Image
+            src="assets/svg/jestj.svg"
+            alt="Jest Icon"
+            width={100}
+            height={100}
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+          />
+          <span className="justify-center text-center text-1xl"></span>
+        </div>
+        <div className="flex flex-col items-center justify-center mt-2">
+          <Image
             src="assets/svg/linux.svg"
             alt="Linux Icon"
+            width={100}
+            height={100}
+            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
+          />
+          <span className="justify-center text-center text-1xl"></span>
+        </div>
+        <div className="flex flex-col items-center justify-center mt-2">
+          <Image
+            src="assets/svg/mongodb.svg"
+            alt="Mongodb Icon"
             width={100}
             height={100}
             className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
@@ -90,16 +110,6 @@ const Tools = () => {
         </div>
         <div className="flex flex-col items-center justify-center mt-2">
           <Image
-            src="assets/svg/neovimio.svg"
-            alt="Neovim Icon"
-            width={100}
-            height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
-          />
-          <span className="justify-center text-1xl"></span>
-        </div>
-        <div className="flex flex-col items-center justify-center mt-2">
-          <Image
             src="assets/svg/postman.svg"
             alt="Postman Icon"
             width={100}
@@ -107,15 +117,6 @@ const Tools = () => {
             className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
           />
           <span className="justify-center text-1xl"></span>
-        </div>
-        <div className="flex flex-col items-center justify-center mt-2">
-          <Image
-            src="assets/svg/vsCode.svg"
-            alt="Vscode Icon"
-            width={100}
-            height={100}
-            className="h-auto max-w-full px-1 py-2 mb-1 bg-white  dark:bg-white rounded text-lg xl:text-4xl border-solid border-2 border-slate-100 aspect-auto object-cover"
-          />
         </div>
       </div>
     </div>
